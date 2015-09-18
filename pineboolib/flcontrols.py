efb88ffb941cfc1d2a4a344ed73e5f4832dcf1fa
José A. Fernández <aullasistemas@gmail.com>
2015-09-18 09:20:26 +0200

    Creado decorador @WorkingOnThis para especificar que algún desarrollador está trabajando en un función

--- pineboolib/flcontrols.py
+++ pineboolib/flcontrols.py
@@ -37,6 +37,14 @@ def NotImplementedWarn(fn):
         return ret
     return newfn
 
+def WorkingOnThis(fn):
+    def newfn(*args,**kwargs):
+        ret = fn(*args,**kwargs)
+        x_args = [ repr(a) for a in args] + [ "%s=%s" % (k,repr(v)) for k,v in list(kwargs.items())]
+        print("WARN: A developer is working on this function : %s(%s) -> %s" % (fn.__name__,", ".join(x_args),repr(ret)))
+        return ret
+    return newfn
+
 class QLayoutWidget(QtGui.QWidget):
     pass
 
