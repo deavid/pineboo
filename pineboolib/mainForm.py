67b470cf5c215bce082d9988e7e070bbf8991ba0
José A. Fernández <aullasistemas@gmail.com>
2016-01-27 22:27:20 +0100

    Solución a  QObject::startTimer: QTimer can only be used with threads started with QThread

--- pineboolib/mainForm.py
+++ pineboolib/mainForm.py
@@ -25,6 +25,7 @@ class MainForm(QtGui.QWidget):
         self.areasTab.removeItem(0) #Borramos tab de ejemplo.
         self.formTab = self.ui.formTab
         self.formTab.setTabsClosable(True)
+        self.setAttribute(QtCore.Qt.WA_DeleteOnClose)
         self.connect(self.formTab, QtCore.SIGNAL('tabCloseRequested(int)'), self.closeFormTab)
         self.formTab.removeTab(0)
         app_icon = QtGui.QIcon()
@@ -37,6 +38,7 @@ class MainForm(QtGui.QWidget):
         app_icon.addFile(filedir('icons/pineboo-logo-256.png'), QtCore.QSize(256,256))
         self.setWindowIcon(app_icon)
         self.setWindowTitle("Pineboo")
+
 
     def closeFormTab(self, numero):
         #print"Cerrando pestaña número %d " % numero
