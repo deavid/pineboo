e10a15264f23c6d54535b9cb7bd70153e19cbea1
José A. Fernández <aullasistemas@gmail.com>
2016-08-16 22:28:20 +0200

    qsatype. Ajuste de dependencias

--- pineboolib/qsatype.py
+++ pineboolib/qsatype.py
@@ -8,11 +8,11 @@ from PyQt4 import QtCore, QtGui
 
 from pineboolib import qsaglobals
 from pineboolib import flcontrols
-from pineboolib import FLFormSearchDB as FLFormSearchDB_legacy
+from pineboolib.fllegacy import FLFormSearchDB as FLFormSearchDB_legacy
 from pineboolib.flcontrols import FLTable, FLReportViewer, QLineEdit
 from pineboolib.fllegacy import FLSqlQuery as FLSqlQuery_Legacy
 from pineboolib.fllegacy import FLSqlCursor as FLSqlCursor_Legacy
-from pineboolib import FLTableDB as FLTableDB_Legacy
+from pineboolib.fllegacy import FLTableDB as FLTableDB_Legacy
 from pineboolib.fllegacy import FLUtil as FLUtil_Legacy
 
 def Object(x=None):
