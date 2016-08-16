3c999c006a26b5ee61bfd4e063a24cbff0531ad5
José A. Fernández <aullasistemas@gmail.com>
2016-08-16 22:21:38 +0200

    CursorTableModel: Adaptado para el uso de clases FL

--- pineboolib/CursorTableModel.py
+++ pineboolib/CursorTableModel.py
@@ -7,6 +7,9 @@ import pineboolib
 
 from PyQt4 import QtCore
 from pineboolib.fllegacy.FLSqlQuery import FLSqlQuery
+from pineboolib.fllegacy.FLFieldMetaData import FLFieldMetaData
+from pineboolib.fllegacy.FLTableMetaData import FLTableMetaData
+import traceback
 
 class CursorTableModel(QtCore.QAbstractTableModel):
     rows = 15
@@ -21,25 +24,30 @@ class CursorTableModel(QtCore.QAbstractTableModel):
         self._prj = project
         if action and action.table:
             self._table = project.tables[action.table]
+            project.conn.manager().metadata(self._table.name)
         else:
             raise AssertionError
         self.sql_fields = []
         self.field_aliases = []
         self.field_type = []
-        for field in self._table.fields:
+        self.field_metaData = []
+        #for field in self._table.fields:
             #if field.visible_grid:
-            self.sql_fields.append(field.name)
-            self.field_aliases.append(aqtt(field.alias))
-            self.field_type.append(field.mtd_type)    
-            if field.pk: self._pk = field.name
-
+            #self.sql_fields.append(field.name())
+            #self.field_metaData.append(field)
+        #    self.tableMetadata().addField(field)
         self._data = []
-        self.cols = len(self.sql_fields)
+        self.cols = len(self.tableMetadata().fieldListObject())
         self.rows = 0
         self.where_filters = {}
         self.refresh()
+        
+    
+    def metadata(self):
+        return self._prj.conn.manager().metadata(self._table.name)
 
     def refresh(self):
+        #print("refrescando modelo tabla %s" % self.tableMetadata().name())
         parent = QtCore.QModelIndex()
         oldrows = self.rows
         self.beginRemoveRows(parent, 0, oldrows )
@@ -58,7 +66,12 @@ class CursorTableModel(QtCore.QAbstractTableModel):
         # FIXME: Cuando la tabla es una query, aquí hay que hacer una subconsulta.
         # FIXME: Agregado limit de 5000 registros para evitar atascar pineboo
         # TODO: Convertir esto a un cursor de servidor
-        sql = """SELECT %s FROM %s WHERE %s LIMIT 5000""" % (", ".join(self.sql_fields),self._table.name, where_filter)
+        sql_fields = []
+        for field in self.tableMetadata().fieldListObject():
+            #if field.visibleGrid():
+            #    sql_fields.append(field.name())
+            sql_fields.append(field.name())
+        sql = """SELECT %s FROM %s WHERE %s LIMIT 5000""" % (", ".join(sql_fields),self.tableMetadata().name(), where_filter)
         self._cursor.execute(sql)
         self.rows = 0
         self.endRemoveRows()
@@ -66,7 +79,7 @@ class CursorTableModel(QtCore.QAbstractTableModel):
             self.rowsRemoved.emit(parent, 0, oldrows - 1)
         newrows = self._cursor.rowcount
         self.beginInsertRows(parent, 0, newrows - 1)
-        print("QUERY:", sql)
+        #print("QUERY:", sql)
         self.rows = newrows
         self._data = []
         for row in self._cursor:
@@ -75,12 +88,33 @@ class CursorTableModel(QtCore.QAbstractTableModel):
         topLeft = self.index(0,0)
         bottomRight = self.index(self.rows-1,self.cols-1)
         self.dataChanged.emit(topLeft,bottomRight)
-        print("rows:", self.rows)
+        #print("rows:", self.rows)
 
     def value(self, row, fieldname):
+        if row < 0 or row >= self.rows: return None
+        
+        col = self.metadata().indexPos(fieldname)
+        campo = self._data[row][col]
+        
+        if self.metadata().field(fieldname).type() == "pixmap":
+            q = FLSqlQuery()     
+            consulta = "SELECT contenido FROM fllarge WHERE refkey ='%s'" % campo
+            q.setSql(consulta)
+            q.exec_()
+            q.first()
+            return q.value(0)
+        else:
+            return campo
+            
+        
+        """
         value = None
         if row < 0 or row >= self.rows: return value
-        col = self.sql_fields.index(fieldname)
+        try:
+            #col = self.sql_fields.index(fieldname)
+            col = self._prj.conn.manager.metadata(self._table.name).fieldIndex(fieldname)
+        except:
+            return value
         if self.field_type[col] == 'pixmap':
             campo = self._data[row][col]
             cur = pineboolib.project.conn.cursor()
@@ -91,19 +125,24 @@ class CursorTableModel(QtCore.QAbstractTableModel):
         else:
             value = self._data[row][col]
         return value
+        """
 
     @decorators.NotImplementedWarn
     def setValue(self,fieldname, value):
         return True
 
     def pK(self): #devuelve el nombre del campo pk
-        return self._pk
+        return self.metadata().pK()
+        #return self._pk
 
     def fieldType(self, fieldName): # devuelve el tipo de campo
+        self.tableMetadata().fieldIsIndex(fieldName)
+        return self.tableMetadata().field(fieldName).type()
+        """
         value = None
         try:
             if not fieldName is None:
-                value = self.field_type[self.sql_fields.index(fieldName)]
+                value = self.field_metaData[self.sql_fields.index(fieldName)].type()
             else:
                 value = None
             return value
@@ -111,31 +150,41 @@ class CursorTableModel(QtCore.QAbstractTableModel):
             print("CursorTableModel: No se encuentra el campo %s" % fieldName)
             return None
             
-    
+        """
     def alias(self, fieldName):
+        return self.tableMetadata().field(fieldName).alias()
+        """
         value = None
         try:
-            value = self.field_aliases[self.sql_fields.index(fieldName)]
+            value = self.field_metaData[self.sql_fields.index(fieldName)].alias()
             return value
         except:
             return value
-        
+        """
     def columnCount(self, parent = None):
         if parent is None: parent = QtCore.QModelIndex()
         if parent.isValid(): return 0
-        #print "colcount", self.cols
+        #print(self.cols)
+        #print("colcount", self.cols)
         return self.cols
 
     def rowCount(self, parent = None):
         if parent is None: parent = QtCore.QModelIndex()
         if parent.isValid(): return 0
-        #print "rowcount", self.rows
+        #print("rowcount", self.rows)
         return self.rows
 
     def headerData(self, section, orientation, role):
         if orientation == QtCore.Qt.Horizontal:
             if role == QtCore.Qt.DisplayRole:
-                return "%s" % self.field_aliases[section]
+                #if self.field_metaData[section].visibleGrid():
+                #    return "%s" % self.field_metaData[section].alias()
+                #else:
+                #    return None
+                #return self.field_metaData[section].alias()
+                alias = str(self.tableMetadata().indexFieldObject(section).alias())
+                #print(alias)
+                return alias
         return QtCore.QAbstractTableModel.headerData(self, section, orientation, role)
 
     def data(self, index, role = QtCore.Qt.DisplayRole):
@@ -153,3 +202,20 @@ class CursorTableModel(QtCore.QAbstractTableModel):
                 raise
 
         return None
+    
+    def fieldMetadata(self, fieldName):
+        return self.tableMetadata().field(fieldName)
+        """
+        try:
+            pos = self.field_metaData(fieldName)
+            return self.field_metaData[pos]
+        except:
+            return False
+            #print("CursorTableModel: %s.%s no hay datos" % ( self._table.name, fieldName ))
+        """
+    def tableMetadata(self):
+        return self._prj.conn.manager().metadata(self._table.name)
+        
+    
+        
+        
