383c2c08aa021374034c66c6618af4abe4c1d414
José A. Fernández <aullasistemas@gmail.com>
2016-09-08 01:27:44 +0200

    FLFormSearchDB. Avances para levantarlo desde QSA

--- pineboolib/fllegacy/FLFormSearchDB.py
+++ pineboolib/fllegacy/FLFormSearchDB.py
@@ -5,36 +5,109 @@ from pineboolib.fllegacy.FLSqlCursor import FLSqlCursor
 from pineboolib import decorators
 from pineboolib.utils import DefFun
 from pineboolib import project
+from PyQt4 import QtCore, QtGui, Qt
+from pineboolib.utils import filedir
 
 class FLFormSearchDB( FLFormDB ):
     _accepted = None
     _cursor = None
+    
+    formReady = QtCore.pyqtSignal()
 
-    def __init__(self,name):
-        self._cursor= FLSqlCursor(name)
-        super(FLFormSearchDB,self).__init__(None,self._cursor.action())
-        self._accepted = False
+    def __init__(self, *args, **kwargs):
+        parent = None
+        name = None
         
-
+        if isinstance(args[0], str):
+            name = args[0]        
+            self._cursor= FLSqlCursor(name)
+            action = self._cursor.action()
+            self._accepted = False
+        
+        elif isinstance(args[0], FLSqlCursor):
+            self.cursor_ = args[0]
+            action = args[1]
+            parent = args[2]
+                 
+        super(FLFormSearchDB,self).__init__(parent ,action)
+        
+        
+    def __delattr__(self, *args, **kwargs):
+        if self.cursor_:
+            self.cursor_.restoreEditionFlag(self)
+            self.cursor_.restoreBrowseFlag(self)
+        
+        FLFormDB.__delattr__(self, *args, **kwargs)
+    
     def __getattr__(self, name): return DefFun(self, name)
+    
+    def setFilter(self, f):
+        
+        if not self.cursor_:
+            return 
+        previousF = self.cursor_.mainFilter()
+        newF = None
+        if not previousF:
+            newF = f
+        elif previousF.contains(f):
+            return
+        else:
+            newF = "%s AND %s" % (previousF, f)
+        self.cursor_.setMainFilter(newF)
+        
 
     def setCursor(self, cursor):
         print("Definiendo cursor")
         self._cursor = cursor
-
-
-    def setMainWidget(self):
+    
+    @decorators.Incomplete
+    def setMainWidget(self, w = None):
         print("Creamos la ventana")
+        if not self._cursor or not w:
+            return
+        
+        if self.showed:
+            if self.mainWidget_ and not self.mainWidget_ == w:
+                self.initMainWidget(w)
+        else:
+            w.hide()
+        
+        if self.layoutButtons:
+            del self.layourButtons
+        
+        if self.layout:
+            del self.layout
+        
+        
+        w.setFont(QtGui.qApp.font())
+        self.layout = QtGui.QVBoxLayout()
+        self.layout.addWidget(w)
+        self.layoutButtons = QtGui.QHBoxLayout()
+        
+        #pbSize = Qt.QSize(22,22)
+        
+        wt = QtGui.QToolButton.whatsThis()
+        wt.setIcon(QtGui.QIcon(filedir("icons","gtk-find.png")))
+        self.layoutButtons.addWidget(wt)
+        wt.show()
+        
+        self.mainWidget_ = w
+        
+        self._cursor.setEdition(False)
+        self._cursor.setBrowse(False)
+        self._cursor.recordChoosed.emit(self.acepted)
+        
+        
+        
+        
+        
         
 
     def exec_(self, valor):
         print("Ejecutamos la ventana y esperamos respuesta, introducimos desde y hasta en cursor")
-
-    def setFilter(self):
-        print("configuramos Filtro")
+        self._cursor.setFilter("1=1")
+        
 
     def accepted(self):
         return self._accepted
-
-    def cursor(self):
-        return self._cursor
+    
