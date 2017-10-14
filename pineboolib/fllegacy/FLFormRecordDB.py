5d349f8021334ee9e21c73d66746c9f28266d946
José A. Fernández <aullasistemas@gmail.com>
2017-10-14 02:18:04 +0200

    bugfix

--- pineboolib/fllegacy/FLFormRecordDB.py
+++ pineboolib/fllegacy/FLFormRecordDB.py
@@ -650,7 +650,7 @@ class FLFormRecordDB(FLFormDB):
                 if not caption:
                     caption = self.cursor_.metadata().alias()
                 self.cursor_.transaction()
-                self.setCaptionWidget("Insertar", caption)
+                self.setCaptionWidget(caption)
                 if self.initFocusWidget_:
                     self.initFocusWidget_.setFocus()
                 self.cursor_.refreshBuffer()
