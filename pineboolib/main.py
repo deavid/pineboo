3717cf2b7c910c2bd629c3117918d82029cfb776
José A. Fernández <aullasistemas@gmail.com>
2017-11-04 03:29:22 +0100

    bugfix

--- pineboolib/main.py
+++ pineboolib/main.py
@@ -143,8 +143,8 @@ class Project(object):
         self.conn = PNConnection(self.dbname, self.dbserver.host, self.dbserver.port, self.dbauth.username, self.dbauth.password, self.dbserver.type)
         
         #Se verifica que existen estas tablas
-        self.conn.createTable(self.conn.manager().createSystemTable("flareas"))
-        self.conn.createTable(self.conn.manager().createSystemTable("flfiles"))
+        self.conn.manager().createSystemTable("flareas")
+        self.conn.manager().createSystemTable("flfiles")
         
         util = FLUtil()
         util.writeSettingEntry(u"DBA/lastDB",self.dbname)
