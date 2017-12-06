d62256c4444c27e60142d93fa59283b858483313
José A. Fernández <aullasistemas@gmail.com>
2017-12-06 23:33:40 +0100

    mejoras en mid

--- pineboolib/flparser/pytnyzer.py
+++ pineboolib/flparser/pytnyzer.py
@@ -771,8 +771,8 @@ class Member(ASTPython):
                         arguments = ["%s[(len(%s) - (%s)):]" % (".".join(part1),".".join(part1), value)] + part2
                     elif member == "mid":
                         value = arg[4:]
-                        value = value[0:len(value) - 1]
                         if value.find(",") > -1:
+                            value = value[0:len(value) - 1]
                             v0 = value[0:value.find(",")]
                             v1 = value[value.find(",") + 1:]
                             arguments = ["%s[%s:%s + %s]" % (".".join(part1), v0, v0, v1)] + part2
