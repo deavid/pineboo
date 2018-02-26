8566089b1053d7e5cc1fa8a8ece1b4166218d427
José A. Fernández <aullasistemas@gmail.com>
2018-02-26 22:56:02 +0100

    downloadManager y download_files

--- pineboolib/utils.py
+++ pineboolib/utils.py
@@ -4,6 +4,8 @@ import os.path
 import re
 import logging
 import sys
+
+
 logger = logging.getLogger(__name__)
 
 
@@ -31,14 +33,10 @@ def filedir(*path):
             Filedir devuelve la ruta absoluta resultado de concatenar los paths que se le pasen y aplicarlos desde la ruta del proyecto.
             Es útil para especificar rutas a recursos del programa.
     """
-    ruta_ = os.path.realpath(os.path.join(os.path.dirname(__file__), *path))
-
-    """
-    Esto es para cuando está compilado, para poder acceder a ficheros fuera del ejecutable
-    """
-    if ruta_.find(":/") > -1:
-        ruta_ = ruta_.replace(":/", "")
-
+    ruta_ = os.path.join(os.path.dirname(__file__), *path)
+    if ruta_.find(":/pineboolib/forms") > -1 or ruta_.find(":/pineboolib/plugins") > -1:
+        ruta_ = ruta_.replace(":", ".")
+    print("--->", ruta_)
     return ruta_
 
 
@@ -170,3 +168,157 @@ def trace_function(f):
         with TraceBlock():
             return f(*args)
     return wrapper
+
+
+from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtCore import QObject, QFileInfo, QFile, QIODevice, QUrl, QByteArray
+from PyQt5.QtNetwork import QNetworkAccessManager, QNetworkReply,\
+    QNetworkRequest
+
+
+class downloadManager(QObject):
+    manager = None
+    currentDownload = None
+    reply = None
+    url = None
+    result = None
+    le = None
+
+    def __init__(self):
+        super(downloadManager, self).__init__()
+        self.manager = QNetworkAccessManager()
+        self.currentDownload = []
+        self.manager.finished.connect(self.downloadFinished)
+
+    def setLE(self, le):
+        self.le = le
+
+    def doDownload(self):
+        request = QNetworkRequest(QUrl(self.le.text()))
+        self.reply = self.manager.get(request)
+        # self.reply.sslErrors.connect(self.sslErrors)
+        self.currentDownload.append(self.reply)
+
+    def saveFileName(self, url):
+        path = url.path()
+        basename = QFileInfo(path).fileName()
+
+        if not basename:
+            basename = "download"
+
+        if QFile.exists(basename):
+            i = 0
+            basename = basename + "."
+            while QFile.exists("%s%s" % (basename, i)):
+                i = i + 1
+
+            basename = "%s%s" % (basename, i)
+
+        return basename
+
+    def saveToDisk(self, filename, data):
+        fi = "%s/%s" % (self.dir_, filename)
+        file = QFile(fi)
+        if not file.open(QIODevice.WriteOnly):
+            return False
+
+        file.write(data.readAll())
+        file.close()
+
+        return True
+
+    def isHttpRedirect(self, reply):
+        statusCode = reply.attribute(QNetworkRequest.HttpStatusCodeAttribute)
+        return statusCode in [301, 302, 303, 305, 307, 308]
+
+    @QtCore.pyqtSlot(QNetworkReply)
+    def downloadFinished(self, reply):
+        url = reply.url()
+        if not reply.error():
+            if not self.isHttpRedirect(reply):
+                filename = self.saveFileName(url)
+                filename = filename.replace(":", "")
+                self.saveToDisk(filename, reply)
+                self.result = "%s ---> %s" % (url, filedir(filename))
+            else:
+                self.result = "Redireccionado ... :("
+        else:
+            self.result = reply.errorString()
+
+        #infile = QFile(filename)
+        # infile.open(QIODevice.ReadOnly)
+        #uncompress = QByteArray(infile.readAll())
+
+        QtWidgets.QMessageBox.information(None, "AVISO", "%s" % self.result)
+
+    def setPath(self, dir_):
+        self.dir_ = dir_
+
+
+def download_files(dir_, file):
+    import sysconfig
+    from PyQt5.QtWidgets import (QApplication, QLabel, QTreeView, QVBoxLayout,
+                                 QWidget, QLineEdit, QPushButton)
+
+    # Try and import the platform-specific modules.
+    platform_module = "Not available"
+
+    try:
+        import PyQt5.QtAndroidExtras
+        platform_module = 'QtAndroidExtras'
+    except ImportError:
+        pass
+
+    try:
+        import PyQt5.QtMacExtras
+        platform_module = 'QtMacExtras'
+    except ImportError:
+        pass
+
+    try:
+        import PyQt5.QtWinExtras
+        platform_module = 'QtWinExtras'
+    except ImportError:
+        pass
+
+    try:
+        import PyQt5.QtX11Extras
+        platform_module = 'QtX11Extras'
+    except ImportError:
+        pass
+
+    # Try and import the optional products.
+    optional_products = []
+
+    # Create the GUI.
+
+    app = QApplication(sys.argv)
+    QApplication.setStyle("Android")
+    shell = QWidget()
+    shell_layout = QVBoxLayout()
+    try:
+        dm = downloadManager()
+    except Exception:
+        pass
+    header = QLabel("""<h1>PINEBOO</h1>
+    <p>Se necesita una descarga adicional
+    </p>""")
+    header.setWordWrap(True)
+    shell_layout.addWidget(header)
+    urllineedit = QLineEdit()
+    urllineedit.setText('http://192.168.1.106/%s' % file)
+    pb = QPushButton()
+    pb.setText("Descargar")
+    dm.setPath(dir_)
+    dm.setLE(urllineedit)
+    pb.clicked.connect(dm.doDownload)
+    shell_layout.addWidget(urllineedit)
+    shell_layout.addWidget(pb)
+    shell.setLayout(shell_layout)
+
+    # Show the GUI and interact with it.
+    shell.show()
+    app.exec()
+
+    # All done.
+    sys.exit()
