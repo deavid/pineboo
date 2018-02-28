82a0a89e3e5991bb84293e9b926202ce0b37112e
antonio-yeboyebo <antonio@yeboyebo.es>
2018-02-28 17:36:36 +0100

    Mejoras en FLTabWidget

--- pineboolib/flcontrols.py
+++ pineboolib/flcontrols.py
@@ -157,29 +157,56 @@ class FLTable(QtWidgets.QTableWidget):
 class QTabWidget(QtWidgets.QTabWidget):
 
     def setTabEnabled(self, tab, enabled):
-        # print("QTabWidget::setTabEnabled %r : %r" % (tab, enabled))
-        if isinstance(tab, int):
-            if not pineboolib.project._DGI.localDesktop():
-                pineboolib.project._DGI._par.addQueque("%s_setTabEnabled" % self.objectName(), [tab, enabled])
-            return QtWidgets.QTabWidget.setTabEnabled(self, tab, enabled)
-        if isinstance(tab, str):
-            """
-            tabs = [ str(QtWidgets.QTabWidget.tabText(self, i)).lower().replace("&","") for i in range(self.count()) ]
-            try:
-                idx = tabs.index(tab.lower())
-                return QtWidgets.QTabWidget.setTabEnabled(self, idx, enabled)
-            """
-            try:
-                for idx in range(self.count()):
-                    if self.widget(idx).objectName() == tab.lower():
-                        if not pineboolib.project._DGI.localDesktop():
-                            pineboolib.project._DGI._par.addQueque("%s_setTabEnabled" % self.objectName(), [idx, enabled])
-                        return QtWidgets.QTabWidget.setTabEnabled(self, idx, enabled)
 
-            except ValueError:
-                print("ERROR: Tab not found:: QTabWidget::setTabEnabled %r : %r" % (tab, enabled))
-                return False
-        print("ERROR: Unknown type for 1st arg:: QTabWidget::setTabEnabled %r : %r" % (tab, enabled))
+        idx = self.indexByName(tab)
+        if idx is None:
+            return
+
+        if pineboolib.project._DGI.localDesktop():
+            return QtWidgets.QTabWidget.setTabEnabled(self, idx, enabled)
+        else:
+            pineboolib.project._DGI._par.addQueque("%s_setTabEnabled" % self.objectName(), [idx, enabled])
+
+            # try:
+            #     for idx in range(self.count()):
+            #         if self.widget(idx).objectName() == tab.lower():
+            #             if not pineboolib.project._DGI.localDesktop():
+            #                 pineboolib.project._DGI._par.addQueque("%s_setTabEnabled" % self.objectName(), [idx, enabled])
+            #             return QtWidgets.QTabWidget.setTabEnabled(self, idx, enabled)
+
+            # except ValueError:
+            #     print("ERROR: Tab not found:: QTabWidget::setTabEnabled %r : %r" % (tab, enabled))
+            #     return False
+        # print("ERROR: Unknown type for 1st arg:: QTabWidget::setTabEnabled %r : %r" % (tab, enabled))
+
+    def showPage(self, tab):
+
+        idx = self.indexByName(tab)
+        if idx is None:
+            return
+
+        if pineboolib.project._DGI.localDesktop():
+            return QtWidgets.QTabWidget.setCurrentIndex(self, idx)
+        else:
+            pass
+            # pineboolib.project._DGI._par.addQueque("%s_setTabEnabled" % self.objectName(), [idx, enabled])
+
+    def indexByName(self, tab):
+
+        idx = None
+        if isinstance(tab, int):
+            return tab
+        elif not isinstance(tab, str):
+            print("ERROR: Unknown type tab name or index:: QTabWidget %r" % (tab))
+            return None
+
+        try:
+            for idx in range(self.count()):
+                if self.widget(idx).objectName() == tab.lower():
+                    return idx
+        except ValueError:
+            print("ERROR: Tab not found:: QTabWidget, tab name = %r" % (tab))
+        return None
 
 
 class QTable(QtWidgets.QTableWidget):
