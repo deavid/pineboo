9992dfecc31eb0a5f039f2f97db25beb79b6225d
antonio-yeboyebo <antonio@yeboyebo.es>
2018-03-19 19:28:43 +0100

    Input.getOpenFileName con 1 único argumento

--- pineboolib/qsaglobals.py
+++ pineboolib/qsaglobals.py
@@ -45,17 +45,19 @@ class FileDialog(QtWidgets.QFileDialog):
     def getOpenFileName(*args):
         obj = None
         parent = QtWidgets.QApplication.activeModalWidget()
-        if len(args) == 2:
-            obj = QtWidgets.QFileDialog.getOpenFileName(
-                parent, str(args[0]), str(args[1]))
+        if len(args) == 1:
+            obj = QtWidgets.QFileDialog.getOpenFileName(parent, str(args[0]))
+        elif len(args) == 2:
+            obj = QtWidgets.QFileDialog.getOpenFileName(parent, str(args[0]), str(args[1]))
         elif len(args) == 3:
-            obj = QtWidgets.QFileDialog.getOpenFileName(
-                parent, str(args[0]), str(args[1]), str(args[2]))
+            obj = QtWidgets.QFileDialog.getOpenFileName(parent, str(args[0]), str(args[1]), str(args[2]))
         elif len(args) == 4:
-            obj = QtWidgets.QFileDialog.getOpenFileName(parent, str(
-                args[0]), str(args[1]), str(args[2]), str(args[3]))
+            obj = QtWidgets.QFileDialog.getOpenFileName(parent, str(args[0]), str(args[1]), str(args[2]), str(args[3]))
 
-        return obj
+        if obj is None:
+            return None
+
+        return obj[0]
 
     def getExistingDirectory(basedir):
         return "%s/" % QtWidgets.QFileDialog.getExistingDirectory(basedir)
