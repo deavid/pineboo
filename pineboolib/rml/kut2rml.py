709e520419c674fc4ff4016b5ef216435251d46e
José A. Fernández <aullasistemas@gmail.com>
2018-05-08 00:55:35 +0200

    RML. mejoras en CalculatedField

--- pineboolib/rml/kut2rml.py
+++ pineboolib/rml/kut2rml.py
@@ -256,6 +256,15 @@ class kut2rml(object):
                 except Exception:
                     print(traceback.format_exc())
                     return
+            else:
+                if not data:
+                    data = self.xmlData_[0]
+                    if xml.get("Field"):
+                        v = data.get(xml.get("Field"))
+                        if v == "None":
+                            v = ""
+                        text = v
+
             if text and xml.get("DataType") is not None:
                 text = self.calculated(text, xml.get("DataType"), xml.get("Precision"), data)
 
