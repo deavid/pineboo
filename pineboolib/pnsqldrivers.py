4a60579eea239939b0e9a4a06514334b1443b3f6
José A. Fernández <aullasistemas@gmail.com>
2018-07-14 10:12:49 +0200

    Documentación pnsqldrivers

--- pineboolib/pnsqldrivers.py
+++ pineboolib/pnsqldrivers.py
@@ -1,16 +1,27 @@
 # -*- coding: utf-8 -*-
-import importlib
-from pineboolib.utils import filedir
+
 import os
 import logging
-from PyQt5 import QtCore
+import importlib
+
+from pineboolib.utils import filedir
+
+
 logger = logging.getLogger(__name__)
 
+"""
+Esta clase gestiona los diferentes controladores de BD.
+"""
 
-class PNSqlDrivers(QtCore.QObject):
+
+class PNSqlDrivers(object):
     driverName = None
     driver_ = None
     _mobile = None
+
+    """
+    Constructor
+    """
 
     def __init__(self, _DGI=None):
         self._mobile = False
@@ -38,6 +49,12 @@ class PNSqlDrivers(QtCore.QObject):
 
         self.defaultDriverName = "FLsqlite"
 
+    """
+    Apunta hacia un controlador dado.
+    @param driverName. Nombre del controlado que se desea usar.
+    @return True o False.
+    """
+
     def loadDriver(self, driverName):
         if driverName is None:
             logger.info("Seleccionado driver por defecto %s", self.defaultDriverName)
@@ -47,12 +64,18 @@ class PNSqlDrivers(QtCore.QObject):
             "pineboolib.plugins.sql.%s" % driverName.lower())
         self.driver_ = getattr(module_, driverName.upper())()
 
-        if self.driver_:
+        if self.driver():
             # self.driverName = driverName
             logger.info("Driver %s v%s", self.driver().driverName(), self.driver().version())
             return True
         else:
             return False
+
+    """
+    Retorna el Alias de un controlador a partir de su Nombre
+    @param name. Nombre del controlador.
+    @return Alias o None.
+    """
 
     def nameToAlias(self, name):
         name = name.lower()
@@ -60,6 +83,12 @@ class PNSqlDrivers(QtCore.QObject):
             return self.driversdict[name]
         else:
             return None
+
+    """
+    Retorna el Nombre de un controlador a partir de su nombre
+    @param alias. Alias con el que se conoce al controlador
+    @return Nombre o None.
+    """
 
     def aliasToName(self, alias):
         if not alias:
@@ -71,15 +100,32 @@ class PNSqlDrivers(QtCore.QObject):
 
         return None
 
+    """
+    Puerto por defecto que una un controlador.
+    @param alias. Alias del controlador.
+    @return Puerto por defecto.
+    """
+
     def port(self, alias):
         for k, value in self.driversDefaultPort.items():
             if k == alias:
                 return "%s" % value
 
+    """
+    Indica si la BD a la que se conecta el controlador es de tipo escriotrio
+    @param alias. Alias del controlador.
+    @return True o False.
+    """
+
     def isDesktopFile(self, alias):
         for k, value in self.desktopFile.items():
             if k == alias:
                 return value
+
+    """
+    Lista los alias de los controladores disponibles
+    @return lista.
+    """
 
     def aliasList(self):
 
@@ -89,8 +135,18 @@ class PNSqlDrivers(QtCore.QObject):
 
         return list
 
+    """
+    Enlace con el controlador usado
+    @return Objecto controlador
+    """
+
     def driver(self):
         return self.driver_
+
+    """
+    Informa del nombre del controlador
+    @return Nombre del controlador
+    """
 
     def driverName(self):
         return self.driver().name()
