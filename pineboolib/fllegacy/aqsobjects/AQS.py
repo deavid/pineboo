edb5092116c46c2d88b09da9b569bb349c823bc8
José A. Fernández <aullasistemas@gmail.com>
2018-08-03 20:40:48 +0200

    Ampliación AQS

--- pineboolib/fllegacy/aqsobjects/AQS.py
+++ pineboolib/fllegacy/aqsobjects/AQS.py
@@ -1,13 +1,22 @@
 # -*- coding: utf-8 -*-
 from PyQt5.QtGui import QColor, QCloseEvent
+from PyQt5.QtWidgets import QFrame, QLabel, QSizePolicy
+from PyQt5.QtCore import Qt
+
+import logging
+
+logger = logging.getLogger("AQS")
 
 class AQS(object):
 
     Close = None
+    Box = None
+    Plain = None
+    AlignTop = None
 
     def __init__(self):
         self.Close = QCloseEvent
-
+        
     def ColorDialog_getColor(self, color=None, parent=None, name=None):
         from PyQt5.QtWidgets import QColorDialog
         from PyQt5.QtGui import QColor
@@ -20,3 +29,12 @@ class AQS(object):
 
         cL = QColorDialog(color, parent)
         return cL.getColor()
+    
+    def __getattr__(self, name):
+        for lib in [QFrame, QLabel, QSizePolicy, Qt]:
+            ret_ = getattr(lib, name, None)
+            if ret_ is not None:
+                return ret_
+        
+        logger.warn("AQS: No se encuentra el atributo %s", name)
+        
