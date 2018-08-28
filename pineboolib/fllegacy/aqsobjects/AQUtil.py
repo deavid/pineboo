fcbef3d51d09f5864ebbcffb615aeb0cad792110
José A. Fernández <aullasistemas@gmail.com>
2018-08-28 18:23:32 +0200

    cambios en comportamiento AQUtil

--- pineboolib/fllegacy/aqsobjects/AQUtil.py
+++ pineboolib/fllegacy/aqsobjects/AQUtil.py
@@ -1,5 +1,17 @@
 # -*- coding: utf-8 -*-
-from pineboolib.fllegacy.FLUtil import FLUtil
+import logging
 
-class AQUtil(FLUtil):
-    pass
+
+class AQUtil(object):
+
+    util = None
+    logger = None
+
+    def __init__(self):
+        from pineboolib.fllegacy.FLUtil import FLUtil
+        self.util = FLUtil()
+        self.logger = logging.getLogger(__name__)
+
+    def __getattr__(self, name):
+        self.logger.info("Usando function FAKE %s de FLUtil()", name)
+        return getattr(self.util, name)
