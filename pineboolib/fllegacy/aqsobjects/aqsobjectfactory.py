72af5ce4007f300e23cd0aec6fda7936d38c0bae
José A. Fernández <aullasistemas@gmail.com>
2018-08-31 17:49:28 +0200

    AQFormDB

--- pineboolib/fllegacy/aqsobjects/aqsobjectfactory.py
+++ pineboolib/fllegacy/aqsobjects/aqsobjectfactory.py
@@ -5,6 +5,8 @@ from pineboolib.fllegacy.aqsobjects.AQSettings import AQSettings
 from pineboolib.fllegacy.aqsobjects.AQUtil import AQUtil as AQUtil_class
 from pineboolib.fllegacy.aqsobjects.AQSql import AQSql
 from pineboolib.fllegacy.aqsobjects.AQS import AQS as AQS_class
+
+from pineboolib.utils import convertFLAction
 import pineboolib
 
 AQUtil = AQUtil_class()
@@ -12,7 +14,7 @@ AQS = AQS_class()
 
 
 def AQFormDB(action_name, parent, other):
-    from pineboolib.fllegacy.FLAction import FLAction
-    ac = pineboolib.project.conn.manager().action(action_name)
-    print("AC", ac)
-    return pineboolib.project.conn.managerModules().createForm(ac, None, parent, name=None)
+    ac_flaction = pineboolib.project.conn.manager().action(action_name)
+    ac_xml = convertFLAction(ac_flaction)
+    ac_xml.load()
+    return ac_xml.mainform_widget
