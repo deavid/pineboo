c9c9d1f71b0159bf3c2a0f661743725023db932a
José A. Fernández <aullasistemas@gmail.com>
2018-09-06 10:12:21 +0200

    nueva función convert2FLAction. Convierte un str o una xmlAction en FLAction

--- pineboolib/utils.py
+++ pineboolib/utils.py
@@ -568,3 +568,14 @@ def convertFLAction(action):
         return pineboolib.project.actions[action.name()]
     else:
         return None
+
+
+def convert2FLAction(action):
+    name = None
+    if isinstance(action, str):
+        name = str
+    else:
+        name = action.name
+
+    from pineboolib.pncontrolsfactory import aqApp
+    return aqApp.db().manager().action(name)
