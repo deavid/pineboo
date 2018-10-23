45c639c7495112e6c3c3c02df536264c6d0e7459
José A. Fernández <aullasistemas@gmail.com>
2018-10-23 09:12:32 +0200

    Mejoras en Process y Dialog

--- pineboolib/plugins/dgi/dgi_qt.py
+++ pineboolib/plugins/dgi/dgi_qt.py
@@ -412,6 +412,14 @@ class QLabel(QtWidgets.QLabel):
     @QtCore.pyqtSlot(bool)
     def setShown(self, b):
         self.setVisible(b)
+    
+    def getAlign(self):
+        return self.alignment()
+    
+    def setAlign(self, alignment_):
+        self.setAlignment(alignment_)
+        
+    alignment = property(getAlign, setAlign)
 
 
 class QListWidgetItem(QtWidgets.QListWidgetItem):
@@ -1226,8 +1234,101 @@ class RadioButton(QtWidgets.QRadioButton):
         if name == "checked":
             return self.isChecked()
 
+class Process(QtCore.QProcess):
 
-class Dialog(QtWidgets.QDialog):
+    stderr = None
+    stdout = None
+
+    def __init__(self, *args):
+        super(Process, self).__init__()
+        self.readyReadStandardOutput.connect(self.stdoutReady)
+        self.readyReadStandardError.connect(self.stderrReady)
+        self.stderr = None
+        self.normalExit = self.NormalExit
+        self.crashExit = self.CrashExit
+        
+        if args:
+            self.running = False
+            self.setProgram(args[0])
+            argumentos = args[1:]
+            self.setArguments(argumentos)
+
+    def start(self):
+        super(Process, self).start()
+
+    def stop(self):
+        super(Process, self).stop()
+
+    def writeToStdin(self, stdin_):
+        import sys
+        encoding = sys.getfilesystemencoding()
+        stdin_as_bytes = stdin_.encode(encoding)
+        self.writeData(stdin_as_bytes)
+        # self.closeWriteChannel()
+
+    def stdoutReady(self):
+        self.stdout = str(self.readAllStandardOutput())
+
+    def stderrReady(self):
+        self.stderr = str(self.readAllStandardError())
+
+    def readStderr(self):
+        return self.stderr
+
+    def getWorkingDirectory(self):
+        return super(Process, self).workingDirectory()
+
+    def setWorkingDirectory(self, wd):
+        super(Process, self).setWorkingDirectory(wd)
+
+    def getIsRunning(self):
+        return self.state() in (self.Running, self.Starting)
+    
+    def exitcode(self):
+        return self.exitCode()
+
+    def execute(comando):
+        import sys
+        encoding = sys.getfilesystemencoding()
+        pro = QtCore.QProcess()
+        if isinstance(comando, str):
+            comando = comando.split(" ")
+
+        programa = comando[0]
+        argumentos = comando[1:]
+        pro.setProgram(programa)
+        pro.setArguments(argumentos)
+        pro.start()
+        pro.waitForFinished(30000)
+        Process.stdout = pro.readAllStandardOutput().data().decode(encoding)
+        Process.stderr = pro.readAllStandardError().data().decode(encoding)
+
+    running = property(getIsRunning)
+    workingDirectory = property(getWorkingDirectory, setWorkingDirectory)
+    
+    
+
+
+QProcess = Process
+
+class QDialog(QtWidgets.QDialog):
+    
+    def getTitle(self):
+        return self.windowTitle()
+    
+    def setTitle(self, title):
+        self.setWindowTitle(title)
+    
+    def getEnabled(self):
+        return self.isEnabled()
+    
+    def setEnable_(self, enable_):
+        self.setEnabled(enable_)
+
+    caption = property(getTitle, setTitle)
+    enable = property(getEnabled, setEnable_)
+    
+class Dialog(QDialog):
     _layout = None
     buttonBox = None
     okButtonText = None
@@ -1295,7 +1396,7 @@ class GroupBox(QGroupBox):
         self._layout.addWidget(_object)
 
 
-QDialog = QtWidgets.QDialog
+
 
 
 class QVBoxLayout(QtWidgets.QVBoxLayout):
