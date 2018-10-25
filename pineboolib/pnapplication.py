18104c79354d318844e331374fc69cb91e367385
José A. Fernández <aullasistemas@gmail.com>
2018-10-25 13:35:56 +0200

    comprobamos si hay un cursor a asignar antes de asignarlo al FLFormRecordDB sin cursor

--- pineboolib/pnapplication.py
+++ pineboolib/pnapplication.py
@@ -836,7 +836,7 @@ class XMLAction(XMLStruct):
                 self.logger.warn("End of record action load %s (iface:%s ; widget:%s)", self.name, getattr(
                     self.formrecord_widget, "iface", None), getattr(self.formrecord_widget, "widget", None))
         
-        if self.formrecord_widget.cursor() is None:
+        if cursor and self.formrecord_widget.cursor() is None:
             self.formrecord_widget.setCursor(cursor)
         
 
