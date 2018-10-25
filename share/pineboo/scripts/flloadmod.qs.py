d197a181c37379cefd8ce5ab907c46763bdbc7ee
José A. Fernández <aullasistemas@gmail.com>
2018-10-25 08:55:16 +0200

    solución de algunos problemas

--- share/pineboo/scripts/flloadmod.qs.py
+++ share/pineboo/scripts/flloadmod.qs.py
@@ -76,27 +76,20 @@ class FormInternalObj(FormDBWidget):
                     # arg00="SEMI"/><regexchar
                     # arg00="RBRACKET"/></regexbody></Constant></Value>
                     dependencias = dameValor(aF[7]).split(unknownarg)
-
+            
             descripcion = traducirCadena(descripcion, fichero.path, modulo)
             desArea = traducirCadena(desArea, fichero.path, modulo)
             fichIcono = File(ustr(fichero.path, u"/", nombreIcono))
             fichIcono.open(File.ReadOnly)
             icono = fichIcono.read()
-            # DEBUG:: Argument 0 not understood
-            # DEBUG:: <Value><Constant><regexbody><regexchar
-            # arg00="LBRACKET"/><regexchar arg00="ICONST:'0'"/><regexchar
-            # arg00="MINUS"/><regexchar arg00="ICONST:'9'"/><regexchar
-            # arg00="RBRACKET"/><regexchar arg00="PLUS"/><regexchar
-            # arg00="PERIOD"/><regexchar arg00="LBRACKET"/><regexchar
-            # arg00="ICONST:'0'"/><regexchar arg00="MINUS"/><regexchar
-            # arg00="ICONST:'9'"/><regexchar arg00="RBRACKET"/><regexchar
-            # arg00="PLUS"/></regexbody></Constant></Value>
-            versionSys = sys.version().match(unknownarg)
-            if compararVersiones(versionSys, versionMinimaFL) == 2:
-                contVersion = MessageBox.warning(util.translate(u"scripts", u"Este módulo necesita la versión ") + versionMinimaFL + util.translate(u"scripts", u" o superior de la aplicación base,\nactualmente la versión instalada es la ") +
-                                                 sys.version() + util.translate(u"scripts", u".\nFacturaLUX puede fallar por esta causa.\n¿Desea continuar la carga?"), MessageBox.Yes, MessageBox.No)
-                if contVersion == MessageBox.No:
-                    return
+
+            
+            #versionSys = sys.version().match("/ [ 0 - 9 ] + .[ 0 - 9 ] + /")
+            #if compararVersiones(versionSys, versionMinimaFL) == 2:
+            #    contVersion = MessageBox.warning(util.translate(u"scripts", u"Este módulo necesita la versión ") + versionMinimaFL + util.translate(u"scripts", u" o superior de la aplicación base,\nactualmente la versión instalada es la ") +
+            #                                     sys.version() + util.translate(u"scripts", u".\nFacturaLUX puede fallar por esta causa.\n¿Desea continuar la carga?"), MessageBox.Yes, MessageBox.No)
+            #    if contVersion == MessageBox.No:
+            #        return
             if evaluarDependencias(dependencias) == False:
                 return
             if not valorPorClave(u"flareas", u"idarea", ustr(u"idarea = '", area, u"'")):
@@ -158,115 +151,115 @@ class FormInternalObj(FormDBWidget):
             setting = ustr(u"scripts/sys/modLastModule_", sys.nameBD())
             util.writeSettingEntry(setting, nombreFichero)
 
-    def dameValor(self, linea=None):
-        return linea
+def dameValor(self, linea=None):
+    return linea
 
-    def valorPorClave(self, tabla=None, campo=None, where=None):
-        valor = None
-        query = FLSqlQuery()
-        query.setTablesList(tabla)
-        query.setSelect(campo)
-        query.setFrom(tabla)
-        query.setWhere(ustr(where, u";"))
-        query.exec_()
-        if query.next():
-            valor = query.value(0)
-        return valor
+def valorPorClave(tabla=None, campo=None, where=None):
+    valor = None
+    query = FLSqlQuery()
+    query.setTablesList(tabla)
+    query.setSelect(campo)
+    query.setFrom(tabla)
+    query.setWhere(ustr(where, u";"))
+    query.exec_()
+    if query.next():
+        valor = query.value(0)
+    return valor
 
-    def compararVersiones(self, v1=None, v2=None):
-        a1 = None
-        a2 = None
-        if v1 and v2:
-            a1 = v1.split(u".")
-            a2 = v2.split(u".")
-            i = 0
-            while_pass = True
-            while i < len(a1):
-                if not while_pass:
-                    i += 1
-                    while_pass = True
-                    continue
-                while_pass = False
-                if parseInt(a1[i]) > parseInt(a2[i]):
-                    return 1
-                if parseInt(a1[i]) < parseInt(a2[i]):
-                    return 2
-                i += 1
-                while_pass = True
-                try:
-                    i < len(a1)
-                except Exception:
-                    break
-
-        return 0
-
-    def evaluarDependencias(self, dependencias=None):
-        res = None
-        if not dependencias:
-            return True
+def compararVersiones(v1=None, v2=None):
+    a1 = None
+    a2 = None
+    if v1 and v2:
+        a1 = v1.split(u".")
+        a2 = v2.split(u".")
         i = 0
         while_pass = True
-        while i < len(dependencias):
+        while i < len(a1):
             if not while_pass:
                 i += 1
                 while_pass = True
                 continue
             while_pass = False
-            if dependencias[i] == '':
-                continue
-            if sys.isLoadedModule(dependencias[i]) == False:
-                res = MessageBox.warning(util.translate(u"scripts", u"Este módulo depende del módulo ") + dependencias[i] + util.translate(
-                    u"scripts", u", que no está instalado.\nFacturaLUX puede fallar por esta causa.\n¿Desea continuar la carga?"), MessageBox.Yes, MessageBox.No)
-                if res == MessageBox.No:
-                    return False
+            if parseInt(a1[i]) > parseInt(a2[i]):
+                return 1
+            if parseInt(a1[i]) < parseInt(a2[i]):
+                return 2
             i += 1
             while_pass = True
             try:
-                i < len(dependencias)
+                i < len(a1)
             except Exception:
                 break
 
+    return 0
+
+def evaluarDependencias(dependencias=None):
+    res = None
+    if not dependencias:
         return True
-
-    def traducirCadena(self, cadena=None, path=None, modulo=None):
-        if cadena.find(u"QT_TRANSLATE_NOOP") == - 1:
-            return cadena
-        cadena = QString(cadena).mid(41, len(cadena) - 43)
-        nombreFichero = None
-        try:
-            nombreFichero = ustr(path, u"/translations/", modulo, u".", util.getIdioma(), u".ts")
-        except Exception as e:
-            e = traceback.format_exc()
-            return cadena
-
-        if not File.exists(nombreFichero):
-            return cadena
-        fichero = File(nombreFichero)
-        fichero.open(File.ReadOnly)
-        f = fichero.read()
-        xmlTranslations = FLDomDocument()
-        if xmlTranslations.setContent(f):
-            nodeMess = xmlTranslations.elementsByTagName(u"message")
-            i = 0
+    i = 0
+    while_pass = True
+    while i < len(dependencias):
+        if not while_pass:
+            i += 1
             while_pass = True
-            while i < len(nodeMess):
-                if not while_pass:
-                    i += 1
-                    while_pass = True
-                    continue
-                while_pass = False
-                if nodeMess.item(i).namedItem(u"source").toElement().text() == cadena:
-                    traduccion = nodeMess.item(i).namedItem(u"translation").toElement().text()
-                    if traduccion:
-                        cadena = traduccion
+            continue
+        while_pass = False
+        if dependencias[i] == '':
+            continue
+        if sys.isLoadedModule(dependencias[i]) == False:
+            res = MessageBox.warning(util.translate(u"scripts", u"Este módulo depende del módulo ") + dependencias[i] + util.translate(
+                u"scripts", u", que no está instalado.\nFacturaLUX puede fallar por esta causa.\n¿Desea continuar la carga?"), MessageBox.Yes, MessageBox.No)
+            if res == MessageBox.No:
+                return False
+        i += 1
+        while_pass = True
+        try:
+            i < len(dependencias)
+        except Exception:
+            break
+
+    return True
+
+def traducirCadena(cadena=None, path=None, modulo=None):
+    if cadena.find(u"QT_TRANSLATE_NOOP") == - 1:
+        return cadena
+    cadena = QString(cadena).mid(41, len(cadena) - 43)
+    nombreFichero = None
+    try:
+        nombreFichero = ustr(path, u"/translations/", modulo, u".", util.getIdioma(), u".ts")
+    except Exception as e:
+        e = traceback.format_exc()
+        return cadena
+
+    if not File.exists(nombreFichero):
+        return cadena
+    fichero = File(nombreFichero)
+    fichero.open(File.ReadOnly)
+    f = fichero.read()
+    xmlTranslations = FLDomDocument()
+    if xmlTranslations.setContent(f):
+        nodeMess = xmlTranslations.elementsByTagName(u"message")
+        i = 0
+        while_pass = True
+        while i < len(nodeMess):
+            if not while_pass:
                 i += 1
                 while_pass = True
-                try:
-                    i < len(nodeMess)
-                except Exception:
-                    break
+                continue
+            while_pass = False
+            if nodeMess.item(i).namedItem(u"source").toElement().text() == cadena:
+                traduccion = nodeMess.item(i).namedItem(u"translation").toElement().text()
+                if traduccion:
+                    cadena = traduccion
+            i += 1
+            while_pass = True
+            try:
+                i < len(nodeMess)
+            except Exception:
+                break
 
-        return cadena
+    return cadena
 
 
 form = None
