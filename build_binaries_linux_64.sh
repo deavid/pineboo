9193e1e2414412ff8fd4ed5d6e31dad340aa2bdc
José A. Fernández <aullasistemas@gmail.com>
2018-12-06 19:34:50 +0100

    moejroas en limpieza compilado linux_64

--- build_binaries_linux_64.sh
+++ build_binaries_linux_64.sh
@@ -2,8 +2,17 @@
 TARGET="linux-64"
 SYSROOT=""
 
+
 if [ -e "sysroots/$TARGET" ]; then
 SYSROOT="--no-sysroot"
 fi
 python3 ./build-pineboo-binaries.py --target $TARGET $SYSROOT
+
+rm ./builds/$TARGET/*.*
+rm ./builds/$TARGET/Makefile
+rm -Rf ./builds/$TARGET/resources
+mkdir ./builds/$TARGET/extra_libs
+mkdir ./builds/$TARGET/extra_libs/psycopg2
+cp ./external_libs_deploy/lib.linux-x86_64-3.7/psycopg2/* ./builds/$TARGET/extra_libs/psycopg2
+
 
