bd54911f8347ceb2d2cf74a025af627ba7323a3b
José A. Fernández <aullasistemas@gmail.com>
2018-12-07 22:35:07 +0100

    ajustes preparando paquete final

--- build_binaries_linux_64.sh
+++ build_binaries_linux_64.sh
@@ -11,8 +11,5 @@ python3 ./build-pineboo-binaries.py --target $TARGET $SYSROOT
 rm ./builds/$TARGET/*.*
 rm ./builds/$TARGET/Makefile
 rm -Rf ./builds/$TARGET/resources
-mkdir ./builds/$TARGET/extra_libs
-mkdir ./builds/$TARGET/extra_libs/psycopg2
-cp ./external_libs_deploy/lib.linux-x86_64-3.7/psycopg2/* ./builds/$TARGET/extra_libs/psycopg2
 
 
