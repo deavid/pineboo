cf72981f76595feec4c369fa186f47ddb545e73b
José A. Fernández <aullasistemas@gmail.com>
2019-01-16 09:11:33 +0100

    spacer_legacy Expande QGroupBox en vertical

--- pineboolib/plugins/dgi/dgi_qt/dgi_objects/qgroupbox.py
+++ pineboolib/plugins/dgi/dgi_qt/dgi_objects/qgroupbox.py
@@ -17,7 +17,7 @@ class QGroupBox(QtWidgets.QGroupBox):
         self._do_style()
         
         if not settings.readBoolEntry("ebcomportamiento/spacerLegacy", False):
-            self.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Fixed)
+            self.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
 
     def setLineWidth(self, width):
         self._line_width = width
