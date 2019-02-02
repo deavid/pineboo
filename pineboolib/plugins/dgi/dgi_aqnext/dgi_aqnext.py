882183a17f096eb4397c150319de298318566ab5
José A. Fernández <aullasistemas@gmail.com>
2019-02-02 10:37:55 +0100

    ajustes(2)

--- pineboolib/plugins/dgi/dgi_aqnext/dgi_aqnext.py
+++ pineboolib/plugins/dgi/dgi_aqnext/dgi_aqnext.py
@@ -1,11 +1,9 @@
 # # -*- coding: utf-8 -*-
-from pineboo import pineboolib
 
-
-from pineboolib.plugins.dgi.dgi_schema import dgi_schema
-from pineboolib.utils import Struct
-from pineboolib import decorators
-import pineboolib
+from pineboo.pineboolib.plugins.dgi.dgi_schema import dgi_schema
+from pineboo.pineboolib.utils import Struct
+from pineboo.pineboolib import decorators
+import pineboo.pineboolib
 
 
 from importlib import import_module
