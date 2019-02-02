ce05dd89910d921d7b51e0105a8a49ef7cec404a
José A. Fernández <aullasistemas@gmail.com>
2019-02-02 10:16:36 +0100

    DGI_AQNEXT inicio de desarrollo

---
+++ pineboolib/plugins/dgi/dgi_aqnext/dgi_aqnext.py
@@ -0,0 +1,56 @@
+# # -*- coding: utf-8 -*-
+
+from pineboolib.plugins.dgi.dgi_schema import dgi_schema
+from pineboolib.utils import Struct
+from pineboolib import decorators
+import pineboolib
+
+
+from importlib import import_module
+
+
+import traceback
+import logging
+import sys
+
+logger = logging.getLogger(__name__)
+
+
+
+class dgi_aqnext(dgi_schema):
+
+
+    def __init__(self):
+        # desktopEnabled y mlDefault a True
+        super().__init__()
+        self._name = "jsonrpc"
+        self._alias = "JSON-RPC"
+        self.setUseDesktop(True)
+        self.setUseMLDefault(True)
+        self.setLocalDesktop(False)
+        self._mainForm = None
+
+    def extraProjectInit(self):
+        pass
+    
+    def create_app(self):
+        app = QtCore.QCoreApplication(sys.argv)
+        return app
+
+    def setParameter(self, param):
+        self._listenSocket = param
+
+    def mainForm(self):
+        if not self._mainForm:
+            self._mainForm = mainForm()
+        return self._mainForm
+
+
+
+
+    def showWidget(self, widget):
+        self._par.addQueque(
+            "%s_showWidget" % widget.__class__.__module__, self._WJS[widget.__class__.__module__])
+
+    def __getattr__(self, name):
+        return super().resolveObject(self._name, name)
