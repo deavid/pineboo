7b4fba5ac3b29304a9da7d963fd0b88bf7e0d553
José A. Fernández <aullasistemas@gmail.com>
2019-02-05 22:51:00 +0100

    formDBWidget para DGI_AQNEXT

---
+++ pineboolib/plugins/dgi/dgi_aqnext/dgi_objects/formdbwidget.py
@@ -0,0 +1,12 @@
+# # -*- coding: utf-8 -*-
+
+class FormDBWidget(object):
+    """description of class"""
+    def debug(*args):
+        try:
+            print(args)
+        except Exception:
+            pass
+
+    def desktopUI(self):
+        return False
