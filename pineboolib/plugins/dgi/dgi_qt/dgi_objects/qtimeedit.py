66afd66f502e75e0f7434e6d064cbbca01cda42f
José A. Fernández <aullasistemas@gmail.com>
2019-02-08 09:25:28 +0100

    QTimeEdit

--- pineboolib/plugins/dgi/dgi_qt/dgi_objects/qtimeedit.py
+++ pineboolib/plugins/dgi/dgi_qt/dgi_objects/qtimeedit.py
@@ -1,5 +1,22 @@
 # -*- coding: utf-8 -*-
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, Qt
 
 class QTimeEdit(QtWidgets.QTimeEdit):
-    pass
+    
+    def __init__(self, parent = None):
+        super().__init__(parent)
+        
+        self.setDisplayFormat("hh:mm:ss A")
+
+    def setTime(self, time):
+        if not isinstance(time, Qt.QTime):
+            t_list = time.split(":")
+            time = Qt.QTime(int(t_list[0]), int(t_list[1]), int(t_list[2]))
+        super().setTime(time)
+    
+    def getTime(self):
+        return super().time().toString("hh:mm:ss")
+    
+    
+
+    time = property(getTime, setTime)
