6ffa779de26b466af3cbc329a54ddf5a42f386b1
José A. Fernández <aullasistemas@gmail.com>
2019-04-06 20:13:00 +0200

    mejoras remote client. Las conexiones se aislan de manera que se permiten multiples conexiones aisladas de otras hacia un solo eneboo server

--- pineboolib/plugins/sql/flremoteclient.py
+++ pineboolib/plugins/sql/flremoteclient.py
@@ -28,13 +28,13 @@ class cursor_class(object):
     
     def __init__(self, driver, n):
         self.driver_ = driver
-        self.id_ = n
+        self.id_ =  n
         self.current_ = None
         self.last_sql = None
         self.description = None
     
     def __getattr__(self, name):
-        logger.warn("cursor(%s).%s!!", self.id_,name, stack_info = True)
+        logger.warn("cursor(%s).%s !!", self.id_,name, stack_info = True)
     
     def execute(self, sql):
         self.last_sql = sql
@@ -148,6 +148,7 @@ class FLREMOTECLIENT(object):
         return self._dbname
     
     def create_dict(self, fun, data = []): 
+        fun = "%s__%s" % (self.id_, fun)
         return pineboolib.utils.create_dict("dbdata", fun, self.id_, data)
     
     def send_to_server(self, js):
@@ -162,11 +163,17 @@ class FLREMOTECLIENT(object):
             print("%s -> %s\nresult: %s" % (js, self.url, res_))
         return res_
 
-    def connect(self, db_name, db_host, db_port, db_userName, db_password):
+    def connect(self, db_name, db_host, db_port, db_user_name, db_password):
         self._dbname = db_name
+        self.id_ = db_user_name
         self.url = "http://%s:%s/jsonrpc" % (db_host, db_port)
         dict_ = self.create_dict("hello")
-        ret = self.send_to_server(dict_)
+        try:
+            ret = self.send_to_server(dict_)
+        except Exception as exc:
+            print(exc)
+            return False
+            
         server_found = False
         
         
@@ -186,25 +193,19 @@ class FLREMOTECLIENT(object):
         return True
     
     def mismatchedTable(self, *args):
-        return True
+        return False
 
     def __getattr__(self, name):
         return virtual_function(name, self).virtual
     
     def refreshQuery(self, curname, fields, table, where, cursor, conn):
-        self.send_to_server(self.create_dict("refreshQuery", {"cursor_id": cursor.id_, "curname": curname, "fields": fields, "table": table, "where": where}))
-        #print("*", curname)
-        #print("*", fields)
-        #print("*", table)
-        #print("where", where)
-        #print(cursor)
-        #print(conn)
+        self.send_to_server(self.create_dict("refreshQuery", {"cursor_id": cursor.id_, "curname": "%s_%s" % (self.id_, curname), "fields": fields, "table": table, "where": where}))
     
     def refreshFetch(self, number, curname, table, cursor, fields, where_filter):
-        self.send_to_server(self.create_dict("refreshFetch", {"cursor_id": cursor.id_, "curname": curname, "fields": fields, "table": table, "where_filter": where_filter, "number": number}))
+        self.send_to_server(self.create_dict("refreshFetch", {"cursor_id": cursor.id_, "curname": "%s_%s" % (self.id_, curname), "fields": fields, "table": table, "where_filter": where_filter, "number": number}))
         
     def fetchAll(self, cursor, tablename, where_filter, fields, curname):
-        return self.send_to_server(self.create_dict("fetchAll", {"cursor_id": cursor.id_, "tablename": tablename, "where_filter": where_filter, "fields": fields , "curname": curname}))
+        return self.send_to_server(self.create_dict("fetchAll", {"cursor_id": cursor.id_, "tablename": tablename, "where_filter": where_filter, "fields": fields , "curname": "%s_%s" % (self.id_, curname)}))
 
 class virtual_function(object):
     name_ = None
@@ -215,8 +216,8 @@ class virtual_function(object):
         self.driver_ = driver
         
     def virtual(self, *args):
+        #return self.driver_.send_to_server(self.driver_.create_dict("%s_%s" % (self.driver_.conn_.user_name_, self.name_), args))
         return self.driver_.send_to_server(self.driver_.create_dict(self.name_, args))
-        
         
         
         
