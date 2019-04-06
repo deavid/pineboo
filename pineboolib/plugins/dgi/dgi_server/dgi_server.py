5d8e6a060652f32e1b7179a4ec181871e82a9753
José A. Fernández <aullasistemas@gmail.com>
2019-04-06 20:11:23 +0200

    mejoras DGI_SERVER

--- pineboolib/plugins/dgi/dgi_server/dgi_server.py
+++ pineboolib/plugins/dgi/dgi_server/dgi_server.py
@@ -76,100 +76,125 @@ class parser(object):
         return Response(response.json, mimetype='application/json')
     
     
+    @dispatcher.add_method
+    def call_function(*args):
+        dict_ = args[0]
+        func_name = dict_["function"]
+        arguments = dict_["arguments"]
+        from pineboolib.pncontrolsfactory import aqApp
+        return aqApp.call(func_name, arguments)
+        
+        
+    
 
     @dispatcher.add_method
     def dbdata(*args):
         dict_ = args[0]
         from pineboolib.pncontrolsfactory import aqApp
+        list_fun = dict_["function"].split("__")
+        fun_name = list_fun[1]
+        id_conn = list_fun[0]
+        cursor = None
         
-        if dict_["function"] == "execute":
-            if not dict_["arguments"]["cursor_id"] in cursor_dict.keys():
-                cursor_dict[dict_["arguments"]["cursor_id"]] = aqApp.db().driver().conn_.cursor()
+        if fun_name == "hello":
+            aqApp.db().removeConn("%s_remote_client" % id_conn)
+            list_to_delete = []
+            for k in list(cursor_dict.keys()):
+                if k.startswith(id_conn):
+                    cursor_dict[k] = None
+                    del cursor_dict[k]
+                
+        conn = aqApp.db().useConn("%s_remote_client" % id_conn)
+        
+        
+
+        #print("--->", dict_["function"], dict_["arguments"]["cursor_id"] if "cursor_id" in dict_["arguments"] else None)
+        
+        if "cursor_id" in dict_["arguments"]:          
+            if not "%s_%s" % (id_conn, dict_["arguments"]["cursor_id"]) in cursor_dict.keys():
+                cursor_dict["%s_%s" % (id_conn, dict_["arguments"]["cursor_id"])] = conn.cursor()
             
-            #res = []
+            cursor = cursor_dict["%s_%s" % (id_conn, dict_["arguments"]["cursor_id"])]
+            
+        
+        if fun_name == "execute":           
             try:
-                cursor_dict[dict_["arguments"]["cursor_id"]].execute(dict_["arguments"]["sql"])
+                cursor.execute(dict_["arguments"]["sql"])
             #    for data in cursor_dict[dict_["arguments"]["cursor_id"]]:
             #        res.append(data)
                     
             except:
-                print("Error %s  %s %s" % (dict_["arguments"]["cursor_id"], dict_["arguments"]["sql"] , dict_["function"]), traceback.format_exc())
-            
-            return
+                print("Error %s  %s %s" % (dict_["arguments"]["cursor_id"], dict_["arguments"]["sql"] , fun_name), traceback.format_exc())
+
         
-        if dict_["function"] == "fetchone":
+        elif fun_name == "fetchone":
             ret = None
             
             try:
-                ret = cursor_dict[dict_["arguments"]["cursor_id"]].fetchone()
+                ret = cursor.fetchone()
             except:
-                print("Error %s" % dict_["function"], traceback.format_exc())
+                print("Error %s" % fun_name, traceback.format_exc())
             return normalize_data(ret)
         
-        if dict_["function"] == "refreshQuery":
+        elif fun_name == "refreshQuery":
             try:
-                fun = getattr(aqApp.db().driver(), dict_["function"])
-                if not dict_["arguments"]["cursor_id"] in cursor_dict.keys():
-                     cursor_dict[dict_["arguments"]["cursor_id"]] = aqApp.db().driver().conn_.cursor()
+                fun = getattr(conn.driver(), fun_name)
                 
-                fun(dict_["arguments"]["curname"], dict_["arguments"]["fields"], dict_["arguments"]["table"], dict_["arguments"]["where"], cursor_dict[dict_["arguments"]["cursor_id"]], aqApp.db().driver().conn_)
+                fun(dict_["arguments"]["curname"], dict_["arguments"]["fields"], dict_["arguments"]["table"], dict_["arguments"]["where"], cursor, aqApp.db().driver().conn_)
             except:
                 print("Error refreshQuery", traceback.format_exc())
-            return
+
         
-        if dict_["function"] == "refreshFetch":
+        elif fun_name == "refreshFetch":
             try:
-                fun = getattr(aqApp.db().driver(), dict_["function"])
-                if not dict_["arguments"]["cursor_id"] in cursor_dict.keys():
-                     cursor_dict[dict_["arguments"]["cursor_id"]] = aqApp.db().driver().conn_.cursor()
+                fun = getattr(conn.driver(), fun_name)
                      
-                fun(dict_["arguments"]["number"], dict_["arguments"]["curname"], dict_["arguments"]["table"], cursor_dict[dict_["arguments"]["cursor_id"]], dict_["arguments"]["fields"], dict_["arguments"]["where_filter"])
+                fun(dict_["arguments"]["number"], dict_["arguments"]["curname"], dict_["arguments"]["table"], cursor, dict_["arguments"]["fields"], dict_["arguments"]["where_filter"])
             except:
                 print("Error refreshFetch", traceback.format_exc())
-            return
         
-        if dict_["function"] == "fetchAll":
+        elif fun_name == "fetchAll":
             try:
-                fun = getattr(aqApp.db().driver(), dict_["function"])
-                if not dict_["arguments"]["cursor_id"] in cursor_dict.keys():
-                     cursor_dict[dict_["arguments"]["cursor_id"]] = aqApp.db().driver().conn_.cursor()
+                fun = getattr(conn.driver(), fun_name)
                      
-                ret = fun(cursor_dict[dict_["arguments"]["cursor_id"]], dict_["arguments"]["tablename"], dict_["arguments"]["where_filter"], dict_["arguments"]["fields"], dict_["arguments"]["curname"])
+                ret = fun( cursor, dict_["arguments"]["tablename"], dict_["arguments"]["where_filter"], dict_["arguments"]["fields"], dict_["arguments"]["curname"])
                 return normalize_data(ret)
             except:
                 print("Error fetchAll", traceback.format_exc())
-            return
+
         
-        if dict_["function"] == "fetchall":
+        elif fun_name == "fetchall":
             try:
-                ret_ =  cursor_dict[dict_["arguments"]["cursor_id"]].fetchall()
+                ret_ =  cursor.fetchall()
                 return normalize_data(ret_)
             except:
                 print("Error fetchall", traceback.format_exc())
         
         
-        if dict_["function"] == "close":
+        elif fun_name == "close":
             try:
-                cursor_dict[dict_["arguments"]["cursor_id"]].close()
+                cursor.close()
                 #print("3 close", dict_["arguments"]["cursor_id"])
-                return
             except:
-                print("Error %s" % dict_["function"], traceback.format_exc())
+                print("Error %s" % fun_name, traceback.format_exc())
         
-        fun = getattr(aqApp.db().driver(), dict_["function"], None)
+        else:
+            
+            
+            fun = getattr(conn.driver(), fun_name, None)
             
                 
-        if fun is None:
-            fun = getattr(parser_server, dict_["function"], None)
+            if fun is None:
+                fun = getattr(parser_server, fun_name, None)
         
-        if fun is not None:
-            expected_args = inspect.getargspec(fun)[0]
-            args_num = len(expected_args)
-            #print("-->", fun, dict_["arguments"][:args_num])
-            return normalize_data(fun(*dict_["arguments"][:args_num]))
+            if fun is not None:
+                expected_args = inspect.getargspec(fun)[0]
+                args_num = len(expected_args)
+                #print("-->", fun, dict_["arguments"][:args_num])
+                return normalize_data(fun(*dict_["arguments"][:args_num]))
         
         
-        return "Desconocido"
+            return "Desconocido"
 
 
 
