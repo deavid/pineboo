7976645bd5c31fbd09d5565057450ff0a2f8151e
José A. Fernández <aullasistemas@gmail.com>
2019-04-09 11:14:38 +0200

    mejoras parseFloat cuando pasamos uns float formateado

--- pineboolib/qsa.py
+++ pineboolib/qsa.py
@@ -253,6 +253,10 @@ def parseFloat(x):
             x += float(list_[1]) / 60 #Minutos a hora
             x += float(list_[2]) / 3600 #Segundos a hora
         
+        if isinstance(x, str):
+            x = x.replace(".", "")
+            x = x.replace(",", ".")
+        
         ret = 0 if x in [None, ""] else float(x) 
         if ret == int(ret):
             ret = int(ret)
