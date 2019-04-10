d416227ea710b214823ca9696100c24f8e089269
José A. Fernández <aullasistemas@gmail.com>
2019-04-10 12:11:51 +0200

    ampliando QComboBox

--- pineboolib/plugins/dgi/dgi_qt/dgi_objects/qcombobox.py
+++ pineboolib/plugins/dgi/dgi_qt/dgi_objects/qcombobox.py
@@ -1,22 +1,19 @@
 # -*- coding: utf-8 -*-
-from PyQt5 import QtWidgets, QtCore
+from PyQt5 import QtWidgets
+from pineboolib import decorators
 
 class QComboBox(QtWidgets.QComboBox):
 
-    _parent = None
 
     def __init__(self, parent=None):
-        self._parent = parent
-        super(QComboBox, self).__init__(parent)
-
-    @property
-    def currentItem(self):
-        return self.currentIndex()
-
-    @currentItem.setter
-    def currentItem(self, i):
-        if i is not None:
-            self.setCurrentIndex(i)
+        super().__init__(parent)  
+    
+        self.setCurrentItem = self.setCurrentIndex
 
     def insertStringList(self, strl):
         self.insertItems(len(strl), strl)
+    
+    @decorators.NotImplementedWarn
+    def __getattr__(self, name):
+        pass
+            
