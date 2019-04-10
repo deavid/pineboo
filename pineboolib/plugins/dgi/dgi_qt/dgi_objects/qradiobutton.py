2f807d0928dcce9d8655e3f6d622702df1ee1aa5
José A. Fernández <aullasistemas@gmail.com>
2019-04-10 12:13:35 +0200

    mejoras QRadioButton

--- pineboolib/plugins/dgi/dgi_qt/dgi_objects/qradiobutton.py
+++ pineboolib/plugins/dgi/dgi_qt/dgi_objects/qradiobutton.py
@@ -3,12 +3,25 @@
 from PyQt5 import QtWidgets
 
 class QRadioButton(QtWidgets.QRadioButton):
+    
+    bg_id = None
 
     def __init__(self, parent = None):
         super().__init__(parent)
         self.setChecked(False)
+        self.bg_id = None
+                
+        self.clicked.connect(self.send_clicked)
         
-        
+    def setButtonGroupId(self, id):  
+        self.bg_id = id
+        if self.parent() and hasattr(self.parent(), "selectedId"):
+            if self.bg_id == self.parent().selectedId:
+                self.setChecked(True)
+    
+    def send_clicked(self):
+        if self.parent() and hasattr(self.parent(), "selectedId"):
+            self.parent().presset.emit(self.dg_id)
         
 
     def __setattr__(self, name, value):
@@ -17,8 +30,10 @@ class QRadioButton(QtWidgets.QRadioButton):
         elif name == "checked":
             self.setChecked(value)
         else:
-            super(RadioButton, self).__setattr__(name, value)
+            super().__setattr__(name, value)
 
     def __getattr__(self, name):
         if name == "checked":
             return self.isChecked()
+        else:
+            print("QRadioButton", name)
