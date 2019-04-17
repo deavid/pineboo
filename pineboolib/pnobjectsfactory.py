a540d55274c1e3c97ceeae70bf0232a4973c46ba
José A. Fernández <aullasistemas@gmail.com>
2019-04-17 08:13:57 +0200

    crea modelos y los mete en el arbol QSA

--- pineboolib/pnobjectsfactory.py
+++ pineboolib/pnobjectsfactory.py
@@ -14,8 +14,8 @@ processed_ = []
 
 
 def base_model( name ):
-    print("Base", name)
-    path = _path("%s.mtd" % name, True)
+    #print("Base", name)
+    path = _path("%s.mtd" % name, False)
     if path:
         path = "%s_model.py" % path[:-4]
         if os.path.exists(path):
@@ -29,32 +29,42 @@ def load_model( nombre ):
     if nombre is None:
         return
     
-    from pineboolib import qsa as qsa_dict_modules
-    db_name = pineboolib.project.conn.DBName()
-    print(2, "load", nombre)
-    if nombre in processed_: 
-        return
+    #if nombre in processed_: 
+    #    return None
     
-    processed_.append(nombre)
+    #processed_.append(nombre)
+    
+    
+    from pineboolib import qsa as qsa_dict_modules
     
     nombre_qsa = nombre.replace("_model", "")
-    model_name = nombre_qsa[0].upper() + nombre_qsa[1:] 
+    model_name = nombre_qsa[0].upper() + nombre_qsa[1:]
+    
+    #mod = getattr(qsa_dict_modules, model_name, None)
+    #if mod is None:
+    #    mod = base_model(nombre)
+    #    if mod:
+    #        setattr(qsa_dict_modules, model_name, mod)
+    
+    
+    
+    db_name = pineboolib.project.conn.DBName()
     
     mod = None
-    module_path = "tempdata.cache.%s.models.%s" % (db_name, nombre)
-    if not os.path.exists(filedir("..", "tempdata", "cache", db_name, "models", nombre)):
-        print("No existe personalizado de", nombre)
-        mod = base_model( nombre )
-    else:
+    file_path = filedir("..", "tempdata", "cache", db_name, "models", "%s_model.py" % nombre)
+    
+    if os.path.exists(file_path):
+        print("Buscando", file_path)
+        module_path = "tempdata.cache.%s.models.%s_model" % (db_name, nombre)
         if module_path in sys.modules:
-            print("Recargando", module_path)
+            #print("Recargando", module_path)
             try:
                 mod = importlib.reload(sys.modules[module_path])
             except Exception as exc:
                 print("** ***", exc)
                 pass 
         else:
-            print("Cargando", module_path)
+            #print("Cargando", module_path)
             try:
                 mod = importlib.import_module(module_path)
             except Exception as exc:
@@ -63,6 +73,10 @@ def load_model( nombre ):
             #models_[nombre] = mod
     
     
+    #if mod:
+    #    setattr(qsa_dict_modules, model_name, mod)
+    
+    #print(3, nombre, mod)
     return mod
        
     #if mod is not None:
@@ -72,18 +86,38 @@ def load_model( nombre ):
     
 
 def load_models():
-    print(1, "load_models!!")
+    #print(1, "load_models!!")
     db_name = pineboolib.project.conn.DBName()
+    tables = pineboolib.project.conn.tables()
     #models_ = {}
+    from pineboolib import qsa as qsa_dict_modules
+    for t in tables:
+        #print(t, "*")
+        mod = base_model(t)
+        #print(t, mod)
+        if mod is not None:
+            model_name = "%s%s" % (t[0].upper(), t[1:])
+            class_ = getattr(mod, model_name, None)
+            if class_ is not None:
+                setattr(qsa_dict_modules, model_name, class_)
+    
+    
+
+    
     for root, dirs, files in os.walk(filedir(".." , "tempdata" , "cache", db_name, "models")):
-        for nombre in files:
+        for nombre in files: #Buscamos los presonalizados
             if nombre.endswith("pyc"):
                 continue
             
-            load_model(nombre)
+            nombre = nombre.replace("_model.py", "")
+            mod = load_model(nombre)
+            if mod is not None:
+                model_name = "%s%s" % (nombre[0].upper(), nombre[1:])
+                class_ = getattr(mod, model_name, None)
+                if class_ is not None:
+                    setattr(qsa_dict_modules, model_name, class_)
             
-            
-            
+               
             
 Calculated = String       
     
