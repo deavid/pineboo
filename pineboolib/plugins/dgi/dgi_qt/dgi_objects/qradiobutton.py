ba93265514a33634d03ebb348328fe0d83cedfd8
José A. Fernández <aullasistemas@gmail.com>
2019-05-13 11:12:22 +0200

    mejoras/limpieza qradiobutton

--- pineboolib/plugins/dgi/dgi_qt/dgi_objects/qradiobutton.py
+++ pineboolib/plugins/dgi/dgi_qt/dgi_objects/qradiobutton.py
@@ -1,39 +1,45 @@
 # -*- coding: utf-8 -*-
 
 from PyQt5 import QtWidgets
+import logging
+from PyQt5.QtCore import pyqtProperty
+
+logger = logging.getLogger(__name__)
 
 class QRadioButton(QtWidgets.QRadioButton):
     
-    bg_id = None
+    dg_id = None
 
     def __init__(self, parent = None):
         super().__init__(parent)
         self.setChecked(False)
-        self.bg_id = None
+        self.dg_id = None
                 
         self.clicked.connect(self.send_clicked)
         
     def setButtonGroupId(self, id):  
-        self.bg_id = id
+        self.dg_id = id
         if self.parent() and hasattr(self.parent(), "selectedId"):
-            if self.bg_id == self.parent().selectedId:
+            if self.dg_id == self.parent().selectedId:
                 self.setChecked(True)
     
     def send_clicked(self):
         if self.parent() and hasattr(self.parent(), "selectedId"):
             self.parent().presset.emit(self.dg_id)
-        
-
-    def __setattr__(self, name, value):
-        if name == "text":
-            self.setText(value)
-        elif name == "checked":
-            self.setChecked(value)
-        else:
-            super().__setattr__(name, value)
-
-    def __getattr__(self, name):
-        if name == "checked":
-            return self.isChecked()
-        else:
-            print("QRadioButton", name)
+    
+    def isChecked(self):
+        return super().isChecked()
+    
+    def setChecked(self, b):
+        super().setChecked(b)
+    
+    def getText(self):
+        return super().getText()
+    
+    def setText(self, t):
+        super().setText(t)
+    
+    checked = pyqtProperty(bool, isChecked, setChecked)
+    text = pyqtProperty(str, getText, setText)
+          
+    
