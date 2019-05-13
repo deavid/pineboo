d3b387055836f72b17ded09dd3a51d865f01417f
José A. Fernández <aullasistemas@gmail.com>
2019-05-13 08:59:19 +0200

    nuevo error a wiki

--- pineboolib/wiki_error.py
+++ pineboolib/wiki_error.py
@@ -19,26 +19,28 @@ def process_error(error_str):
         error = "AttributeError: 'dict' object has no attribute"
         var = error_str[error_str.find(error) + len(error) + 1:]
         var = var.replace("\n","")
-        ret += sys.translate("scripts", "La forma correcta de acceder a .%s es [%s]") % (var, var)
+        ret += sys.translate("scripts", "La forma correcta de acceder a .%s es [%s].") % (var, var)
         
     elif "'builtin_function_or_method' object has no attribute" in error_str:
         error = "'builtin_function_or_method' object has no attribute"
         var = error_str[error_str.find(error) + len(error) + 1:]
         var = var.replace("\n","")
         var = var.replace("'", "")
-        ret += sys.translate("scripts", "La forma correcta de acceder a .%s es ().%s") % (var, var)
+        ret += sys.translate("scripts", "La forma correcta de acceder a .%s es ().%s.") % (var, var)
         
     elif "AttributeError: 'ifaceCtx' object has no attribute" in error_str:
         error = "AttributeError: 'ifaceCtx' object has no attribute"
         var = error_str[error_str.find(error) + len(error) + 1:] 
         var = var.replace("\n","")
         var = var.replace("'", "")
-        ret += sys.translate("scripts", "No se ha traducido el script o el script está vacio")
+        ret += sys.translate("scripts", "No se ha traducido el script o el script está vacio.")
     elif "object is not callable" in error_str:
         error = "object is not callable"
         var = error_str[error_str.find("TypeError") + 10:error_str.find(error)] 
         ret += sys.translate("scripts", "Estas llamando a un objeto %s .Los parentesis finales hay que quitarlos." % var)
-        
+    elif "unsupported operand type(s) for" in error_str:
+        error = "unsupported operand type(s) for"
+        ret += sys.translate("scripts", "No puedes hacer operaciones entre dos \'Nones\' o dos tipos diferentes. Revisa el script y controla esto.")    
     
     else:
         ret += sys.translate("scripts", "Información no disponible.")
