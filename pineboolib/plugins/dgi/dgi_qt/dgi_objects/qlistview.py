233ee160cf0683b13247f1ca17d8b3474b981280
José A. Fernández <aullasistemas@gmail.com>
2019-05-27 10:16:05 +0200

    bugfix: QListView ya muestra correctamente la cabecera. Falta items

--- pineboolib/plugins/dgi/dgi_qt/dgi_objects/qlistview.py
+++ pineboolib/plugins/dgi/dgi_qt/dgi_objects/qlistview.py
@@ -1,36 +1,54 @@
 # -*- coding: utf-8 -*-
 
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtGui
 from pineboolib import decorators
 
 class QListView(QtWidgets.QWidget):
 
     _resizeable = True
     _clickable = True
+    _tree = None
+    _cols_labels = None
     
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        pass
+    def __init__(self, parent = None):
+        super().__init__(parent = None)
+        lay = QtWidgets.QVBoxLayout(self)
+        self._tree = QtWidgets.QTreeView(self)
+        lay.addWidget(self._tree)
+        self._tree.setModel(QtGui.QStandardItemModel())
+        self._cols_labels = []
     
     @decorators.NotImplementedWarn
     def addItem(self, item):
         pass
 
+
     @decorators.NotImplementedWarn
     def setItemMargin(self, m):
         self.setContentsMargins(m, m, m, m)
     
-    @decorators.NotImplementedWarn
-    def setHeaderLabel(self, l):
-        pass
+    def setHeaderLabel(self, labels):
+        if isinstance(labels, str):
+            labels = [labels]
+        
+        self._tree.model().setHorizontalHeaderLabels(labels)  
+        self._cols_labels = labels
+        
+            
     
-    @decorators.NotImplementedWarn
-    def setColumnText(self, col, text):
-        pass
+    def setColumnText(self, col, new_value):
+        i = 0
+        new_list = []
+        for old_value in self._cols_labels:
+            value = new_value if i == col else old_value
+            new_list.append(value)
+        
+        self._cols_labels = new_list
     
-    @decorators.NotImplementedWarn
     def addColumn(self, text):
-        pass
+        self._cols_labels.append(text)
+        
+        self.setHeaderLabel(self._cols_labels)
     
     @decorators.NotImplementedWarn
     def setExpandable(self, e):
@@ -67,4 +85,4 @@ class QListView(QtWidgets.QWidget):
     
     @decorators.NotImplementedWarn
     def clear(self):
-        pass
+        self._cols_labels = []
