6342aff5a815b492ecd5d368163a3057381cb51d
José A. Fernández <aullasistemas@gmail.com>
2019-05-28 11:31:07 +0200

    avances FLListView y FLListViewItems

---
+++ pineboolib/plugins/dgi/dgi_qt/dgi_objects/fllistviewitem.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+
+from PyQt5 import Qt
+from pineboolib import decorators
+import logging
+logger = logging.getLogger("FLListViewItem")
+
+class FLListViewItem(Qt.QStandardItem):
+    
+    _expandable = None
+    _key = None
+    _open = None
+    _parent_model = None
+    
+    def __init__(self, parent = None):
+        super().__init__()
+        self._key = ""
+        self._open = False
+        if parent:
+            self._parent_model = parent.model()
+            if parent.model().item(0,0) is not None:
+                parent.model().item(0,0).setChild(0, self)
+            else:
+                parent.model().setItem(0,0,self)
+            
+    
+    @decorators.NotImplementedWarn
+    def setText(self, *args):
+        #logger.warning("Seteo texto %s" , args, stack_info = True )
+        pos = 0
+        if len(args) == 1:
+            value = args[0]
+        else:
+            pos = args[0]
+            value = args[1]
+        
+        if pos == 0:
+            super().setText(value)
+            
+
+    @decorators.NotImplementedWarn
+    def text(self, col):
+        ret = ""
+        if col == 0:
+            ret = super().text()
+        
+        return str(ret)
+    
+    @decorators.NotImplementedWarn
+    def setPixmap(self, *args):
+        pass
+    
+    def setExpandable(self, b):
+        self._expandable = b
+    
+    
+    def setKey(self, k):
+        self._key = str(k)
+    
+    def key(self):
+        return self._key
+    
+    def setOpen(self, o):
+        self._open = o
+    
+ 
--- pineboolib/plugins/dgi/dgi_qt/dgi_objects/qlistview.py
+++ pineboolib/plugins/dgi/dgi_qt/dgi_objects/qlistview.py
@@ -2,13 +2,19 @@
 
 from PyQt5 import QtWidgets, QtGui
 from pineboolib import decorators
+import pineboolib
 
 class QListView(QtWidgets.QWidget):
 
     _resizeable = True
     _clickable = True
+    _root_is_decorated = None
+    _default_rename_action = None
     _tree = None
     _cols_labels = None
+    _key = None
+    _root_item = None
+    _current_row = None
     
     def __init__(self, parent = None):
         super().__init__(parent = None)
@@ -17,11 +23,20 @@ class QListView(QtWidgets.QWidget):
         lay.addWidget(self._tree)
         self._tree.setModel(QtGui.QStandardItemModel())
         self._cols_labels = []
+        self._root_is_decorated = False
+        self._key = ""
+        self._root_item = None
+        self._current_row = -1
     
     @decorators.NotImplementedWarn
-    def addItem(self, item):
+    def addItem(self, t):
+    #    from pineboolib.pncontrolsfactory import FLListViewItem
+    #    self._current_row = self._current_row + 1
+    #    item = FLListViewItem(self)
+    #    item.setText(t)
+    #    print("PADRE", item)
+    #    self._tree.model().setItem(self._current_row, 0, item)
         pass
-
 
     @decorators.NotImplementedWarn
     def setItemMargin(self, m):
@@ -35,7 +50,7 @@ class QListView(QtWidgets.QWidget):
         self._cols_labels = labels
         
             
-    
+    @decorators.NotImplementedWarn
     def setColumnText(self, col, new_value):
         i = 0
         new_list = []
@@ -50,30 +65,11 @@ class QListView(QtWidgets.QWidget):
         
         self.setHeaderLabel(self._cols_labels)
     
-    @decorators.NotImplementedWarn
-    def setExpandable(self, e):
-        pass
-    
-    @decorators.NotImplementedWarn
-    def setText(self, *args):
-        pass
-    
-    @decorators.NotImplementedWarn
-    def text(self, pos):
-        pass
-    
-    @decorators.NotImplementedWarn
-    def setOpen(self, b):
-        pass
-    
-    
-    @decorators.NotImplementedWarn
-    def setPixmap(self, *args):
-        pass
 
     @decorators.NotImplementedWarn
     def setClickable(self, c):
         self._clickable = True if c else False
+    
 
     @decorators.NotImplementedWarn
     def setResizable(self, r):
@@ -86,3 +82,27 @@ class QListView(QtWidgets.QWidget):
     @decorators.NotImplementedWarn
     def clear(self):
         self._cols_labels = []
+    
+    @decorators.NotImplementedWarn
+    def defaultRenameAction(self):
+        return self._default_rename_action
+    
+    @decorators.NotImplementedWarn
+    def setDefaultRenameAction(self, b):
+        self._default_rename_action = b
+    
+    
+    def model(self):
+        return self._tree.model()
+    
+    #def __getattr__(self, name):
+    #    att = getattr(self._tree, name, None)
+    #    return att
+    
+    #def key(self):
+    #    return self._key
+    
+    #def setKey(self, k):
+    #    self._key = str(k)
+
+            
