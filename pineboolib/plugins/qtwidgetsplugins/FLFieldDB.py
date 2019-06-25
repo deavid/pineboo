cde8346943a04369f6b6da58d76470fe9931ca8d
José A. Fernández <aullasistemas@gmail.com>
2019-06-25 09:18:51 +0200

    documentar pluginType = MODULE

--- pineboolib/plugins/qtwidgetsplugins/FLFieldDB.py
+++ pineboolib/plugins/qtwidgetsplugins/FLFieldDB.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-pluginType = MODULE  # noqa  # FIXME: Esto no es Python.
+pluginType = MODULE  # noqa  # La constante MODULE es parte de cómo PyQt carga los plugins. Es insertada por el loader en el namespace local
 
 
 def moduleInformation():
--- pineboolib/plugins/qtwidgetsplugins/FLTable.py
+++ pineboolib/plugins/qtwidgetsplugins/FLTable.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-pluginType = MODULE  # noqa  
+pluginType = MODULE  # noqa  # La constante MODULE es parte de cómo PyQt carga los plugins. Es insertada por el loader en el namespace local
 
 
 def moduleInformation():
