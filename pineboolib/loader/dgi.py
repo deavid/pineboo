a09c9e93cafab61e9d1b640cfd72a373e914e299
David Martinez Marti <deavidsedice@gmail.com>
2019-06-28 00:43:00 +0100

    remove dynamic import machinery from dgi

--- pineboolib/loader/dgi.py
+++ pineboolib/loader/dgi.py
@@ -1,4 +1,3 @@
-import importlib
 import logging
 
 logger = logging.getLogger(__name__)
@@ -7,30 +6,12 @@ logger = logging.getLogger(__name__)
 def load_dgi(name, param):
     """Load a DGI module dynamically."""
 
-    modname = "dgi_%s" % name
-    modpath = "pineboolib.plugins.dgi.%s.%s" % (modname, modname)
-
-    # FIXME: No se puede importar el proyecto si aún ni lo hemos cargado.
-    # try:
-    #     import pineboolib  # noqa: F401
-    # except Exception:
-    #     print("Cargando como invitado")
-    #     modpath = "pineboo.%s" % modpath
-
-    try:
-        dgi_pymodule = importlib.import_module(modpath)
-    except ImportError:
-        print("No se ha encontrado el módulo DGI %s" % (modpath))
-        raise
-
-    dgi_entrypoint = getattr(dgi_pymodule, modname, None)
-    if dgi_entrypoint is None:
-        raise ImportError("Fallo al cargar el punto de entrada al módulo DGI %s" % modpath)
+    dgi_entrypoint = DGILoader.load_dgi(name)
 
     try:
         dgi = dgi_entrypoint()  # FIXME: Necesitamos ejecutar código dinámico tan pronto?
     except Exception:
-        logger.exception("Error inesperado al cargar el módulo DGI %s" % modpath)
+        logger.exception("Error inesperado al cargar el módulo DGI %s" % name)
         raise
 
     if param:
@@ -39,3 +20,42 @@ def load_dgi(name, param):
     logger.info("DGI loaded: %s", name)
 
     return dgi
+
+
+class DGILoader(object):
+    @staticmethod
+    def load_dgi_qt():
+        from pineboolib.plugins.dgi.dgi_qt import dgi_qt as dgi
+
+        return dgi.dgi_qt
+
+    @staticmethod
+    def load_dgi_aqnext():
+        from pineboolib.plugins.dgi.dgi_aqnext import dgi_aqnext as dgi
+
+        return dgi.dgi_aqnext
+
+    @staticmethod
+    def load_dgi_fcgi():
+        from pineboolib.plugins.dgi.dgi_fcgi import dgi_fcgi as dgi
+
+        return dgi.dgi_fcgi
+
+    @staticmethod
+    def load_dgi_jsonrpc():
+        from pineboolib.plugins.dgi.dgi_jsonrpc import dgi_jsonrpc as dgi
+
+        return dgi.dgi_jsonrpc
+
+    @staticmethod
+    def load_dgi_server():
+        from pineboolib.plugins.dgi.dgi_server import dgi_server as dgi
+
+        return dgi.dgi_server
+
+    @classmethod
+    def load_dgi(cls, name):
+        loader = getattr(cls, "load_dgi_%s" % name, None)
+        if not loader:
+            raise ValueError("Unknown DGI %s" % name)
+        return loader
