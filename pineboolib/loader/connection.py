d55fdf80f6e1ddc08b5d94616081259179c9cb20
José A. Fernández <aullasistemas@gmail.com>
2019-06-28 19:06:24 +0200

    ajustes de refactorización

--- pineboolib/loader/connection.py
+++ pineboolib/loader/connection.py
@@ -4,7 +4,7 @@ DEFAULT_SQLITE_CONN = ProjectConfig(database="pineboo.sqlite3", type="SQLite3 (S
 
 
 def config_dbconn(options):
-    from pineboolib.core.utils import filedir
+    from pineboolib.core.utils.utils_base import filedir
     from .projectconfig import ProjectConfig
 
     if options.project:  # FIXME: --project debería ser capaz de sobreescribir algunas opciones
