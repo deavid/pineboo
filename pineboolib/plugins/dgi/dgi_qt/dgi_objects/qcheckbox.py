db750515db5b75859bdc6a8227ca2be04b74d04b
José A. Fernández <aullasistemas@gmail.com>
2019-07-10 19:35:48 +0200

    cambio en propiedad checked

--- pineboolib/plugins/dgi/dgi_qt/dgi_objects/qcheckbox.py
+++ pineboolib/plugins/dgi/dgi_qt/dgi_objects/qcheckbox.py
@@ -10,15 +10,15 @@ class QCheckBox(QtWidgets.QCheckBox):
         self._parent = parent
         super().__init__(parent)
 
-    @QtCore.pyqtProperty(int)
-    def checked(self):
+    def get_checked(self):
         return self.isChecked()
 
-    @checked.setter
-    def setter_checked(self, b):
+    def set_checked(self, b):
         if isinstance(b, str):
             b = b == "true"
 
         super().setChecked(b)
-        # if not project._DGI.localDesktop():
-        #    project._DGI._par.addQueque("%s_setChecked" % self._parent.objectName(), b)
+
+    checked = property(get_checked, set_checked)
+    # if not project._DGI.localDesktop():
+    #    project._DGI._par.addQueque("%s_setChecked" % self._parent.objectName(), b)
