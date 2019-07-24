ec4cf4d8f052f4db4616a6a8d763d133fdea5f14
José A. Fernández <aullasistemas@gmail.com>
2019-07-24 07:19:40 +0200

    tipeado filedialog

--- pineboolib/plugins/dgi/dgi_qt/dgi_objects/filedialog.py
+++ pineboolib/plugins/dgi/dgi_qt/dgi_objects/filedialog.py
@@ -1,22 +1,22 @@
 # -*- coding: utf-8 -*-
 from PyQt5.QtWidgets import QFileDialog, QApplication  # type: ignore
 import os
-from typing import Any
+from typing import Any, Optional
 
 
 class FileDialog(object):
     @staticmethod
-    def getOpenFileName(*args) -> Any:
+    def getOpenFileName(*args) -> Optional[str]:
         obj = QFileDialog.getOpenFileName(QApplication.activeWindow(), *args)
         return obj[0] if obj is not None else None
 
     @staticmethod
-    def getSaveFileName(filter: str = None, title="Pineboo") -> Any:
+    def getSaveFileName(filter: Optional[str] = None, title: str = "Pineboo") -> Optional[str]:
         ret = QFileDialog.getSaveFileName(QApplication.activeWindow(), title, os.getenv("HOME"), filter)
         return ret[0] if ret else None
 
     @staticmethod
-    def getExistingDirectory(basedir: str = None, title="Pineboo") -> Any:
+    def getExistingDirectory(basedir: Optional[str] = None, title: str = "Pineboo") -> Optional[str]:
         dir_ = basedir if basedir and os.path.exists(basedir) else "%s/" % os.getenv("HOME")
         ret = QFileDialog.getExistingDirectory(QApplication.activeWindow(), title, dir_, QFileDialog.ShowDirsOnly)
         return "%s/" % ret if ret else ret
