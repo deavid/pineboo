e2f74f89a49eae6a914b1a084d179928b3e59dcb
José A. Fernández <aullasistemas@gmail.com>
2019-07-24 07:08:04 +0200

    tipeado FLVar

--- pineboolib/fllegacy/flvar.py
+++ pineboolib/fllegacy/flvar.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
-from pineboolib.fllegacy.flsqlquery import FLSqlQuery
-from pineboolib.fllegacy.flutil import FLUtil
+from pineboolib.application.database.utils import sqlSelect, sqlDelete, sqlUpdate, sqlInsert
 from pineboolib.fllegacy.flapplication import aqApp
 
 from PyQt5 import QtCore  # type: ignore
@@ -8,11 +7,13 @@ from typing import Any
 
 
 class FLVar(object):
-    def set(self, n, v) -> Any:
+    def set(self, n: str, v: Any) -> bool:
+        from pineboolib.application.database.pnsqlquery import PNSqlQuery
+
         id_sesion = aqApp.timeUser().toString(QtCore.Qt.ISODate)
         where = "idvar = '%s' AND idsesion ='%s'" % (n, id_sesion)
 
-        q = FLSqlQuery()
+        q = PNSqlQuery()
         q.setTablesList("flvar")
         q.setSelect("id")
         q.setFrom("flvar")
@@ -22,22 +23,22 @@ class FLVar(object):
         if not q.exec_():
             return False
         if q.next():
-            return FLUtil().sqlUpdate("flvar", "valor", str(v), "id='%s'" % str(q.value(0)))
+            return sqlUpdate("flvar", "valor", str(v), "id='%s'" % str(q.value(0)))
 
         values = "%s,%s,%s" % (n, id_sesion, str(v))
-        FLUtil().sqlInsert("flvar", "idvar,idsesion,idvalor", values)
+        return sqlInsert("flvar", "idvar,idsesion,idvalor", values)
 
-    def get(self, n) -> Any:
+    def get(self, n: str) -> Any:
         id_sesion = aqApp.timeUser().toString(QtCore.Qt.ISODate)
         where = "idvar = '%s' AND idsesion ='%s'" % (n, id_sesion)
-        return FLUtil().sqlSelect("flvar", "valor", where, "flvar")
+        return sqlSelect("flvar", "valor", where, "flvar")
 
-    def del_(self, n) -> Any:
+    def del_(self, n: str) -> bool:
         id_sesion = aqApp.timeUser().toString(QtCore.Qt.ISODate)
         where = "idvar = '%s' AND idsesion ='%s'" % (n, id_sesion)
-        return FLUtil().sqlDelete("flvar", where)
+        return sqlDelete("flvar", where)
 
-    def clean(self) -> Any:
+    def clean(self) -> bool:
         id_sesion = aqApp.timeUser().toString(QtCore.Qt.ISODate)
         where = "idsesion = '%s'" % id_sesion
-        return FLUtil().sqlDelete("flvar", where)
+        return sqlDelete("flvar", where)
