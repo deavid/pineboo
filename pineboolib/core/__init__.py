9add1c178e2e5d8c8569790bc0c7694f7686c90e
David Martinez Marti <deavidsedice@gmail.com>
2019-07-28 10:34:23 +0100

    pydocstyle: decorators.py

---
+++ pineboolib/core/__init__.py
@@ -0,0 +1,5 @@
+"""Core package for utilities.
+
+This package holds all functions and classes that are like side utilities that don't require
+any dependency from other folders. So they're safe to import.
+"""
