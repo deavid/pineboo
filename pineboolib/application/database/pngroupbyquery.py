73b9ad0b92639d08e6f2310c45afd116494e9209
José A. Fernández <aullasistemas@gmail.com>
2019-07-29 17:51:49 +0200

    pydocstyle pngroupbyquery.py

--- pineboolib/application/database/pngroupbyquery.py
+++ pineboolib/application/database/pngroupbyquery.py
@@ -1,14 +1,24 @@
+# -*- coding: utf-8 -*-
+"""Manage a group by a PNSqlQuery instance."""
+
+
 class PNGroupByQuery(object):
+    """Class that manages each group by of a query."""
 
     level_: int
     field_: str
 
     def __init__(self, l: int, f: str) -> None:
+        """Specify the level and field."""
+
         self.level_ = l
         self.field_ = f
 
     def level(self) -> int:
+        """Return the level GroupBy."""
+
         return self.level_
 
     def field(self) -> str:
+        """Return the field GroupBy."""
         return self.field_
