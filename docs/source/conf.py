ec85e3fc72e69b6d2e3bcd01ef4c3d733f216ccd
David Martinez Marti <deavidsedice@gmail.com>
2019-07-30 17:31:47 +0100

    sphynx fixes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,7 +16,9 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath("../.."))
+from pineboolib.loader.main import init_cli  # noqa: F402
 
+init_cli()
 # -- Project information -----------------------------------------------------
 
 project = "Pineboo"
