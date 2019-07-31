62503050d7e0a7fc1d7f5f45879564f1ce73b938
David Martinez Marti <deavidsedice@gmail.com>
2019-07-31 18:42:34 +0100

    sphinx: better gitignore

--- docs/update_linters.sh
+++ docs/update_linters.sh
@@ -4,12 +4,14 @@ APP=$2
 if [ "$LINTER" = "pylint" ]; then
     (cd .. && pylint pineboolib/$APP --load-plugins=pylint_json2html --output-format=jsonextended) \
         | pylint-json2html -f jsonextended -o source/linters/pylint/static/pylint_$APP.html;
+    sed -e 's|body {|#pylint { font-size: 12px;|' -e 's|<body>|<div id="pylint">|' \
+        -e 's|</body>|</div>|' -i source/linters/pylint/static/pylint_$APP.html       
     exit 0;
 fi
 PACKAGES=$( (cd ../pineboolib && find * -maxdepth 0 -type d \! -iname "_*") )
 # ----
 echo "Running PyLint . . ."
-echo $PACKAGES | xargs -t -n1 -P2 $0 pylint
+echo $PACKAGES | xargs -t -n1 -P8 $0 pylint
 # ----
 echo "Running MyPy . . ."
 (cd .. && mypy -p pineboolib --html-report=docs/source/_static/linters/mypy)
