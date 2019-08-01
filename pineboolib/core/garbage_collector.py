8e1ccf4db983de8c2566b4630adabf8f256338df
José A. Fernández <aullasistemas@gmail.com>
2019-08-02 00:52:12 +0200

    garbage_collector se para a core

---
+++ pineboolib/core/garbage_collector.py
@@ -0,0 +1,34 @@
+from typing import Any, Callable, List
+
+
+def check_gc_referrers(typename: Any, w_obj: Callable, name: str) -> None:
+    import threading
+    import time
+
+    def checkfn() -> None:
+        import gc
+
+        time.sleep(2)
+        gc.collect()
+        obj = w_obj()
+        if not obj:
+            return
+        # TODO: Si ves el mensaje a continuación significa que "algo" ha dejado
+        # ..... alguna referencia a un formulario (o similar) que impide que se destruya
+        # ..... cuando se deja de usar. Causando que los connects no se destruyan tampoco
+        # ..... y que se llamen referenciando al código antiguo y fallando.
+        # print("HINT: Objetos referenciando %r::%r (%r) :" % (typename, obj, name))
+        for ref in gc.get_referrers(obj):
+            if isinstance(ref, dict):
+                x: List[str] = []
+                for k, v in ref.items():
+                    if v is obj:
+                        k = "(**)" + k
+                        x.insert(0, k)
+                # print(" - dict:", repr(x), gc.get_referrers(ref))
+            else:
+                if "<frame" in str(repr(ref)):
+                    continue
+                # print(" - obj:", repr(ref), [x for x in dir(ref) if getattr(ref, x) is obj])
+
+    threading.Thread(target=checkfn).start()
