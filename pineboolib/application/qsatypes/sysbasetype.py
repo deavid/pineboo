cc68baf3e5ea01c3fde2908f1321330c12ba7843
David Martinez Marti <deavidsedice@gmail.com>
2019-08-02 18:14:18 +0100

    pydocstyle: fix sysbasetype

--- pineboolib/application/qsatypes/sysbasetype.py
+++ pineboolib/application/qsatypes/sysbasetype.py
@@ -29,10 +29,15 @@ logger = logging.getLogger("fllegacy.systype")
 
 
 class SysBaseType(object):
+    """
+    Obtain useful data from the application.
+    """
+
     time_user_ = QtCore.QDateTime.currentDateTime()
 
     @classmethod
     def nameUser(self) -> str:
+        """Get current database user."""
         ret_ = None
         if project.conn is None:
             raise Exception("Project is not connected yet")
@@ -46,39 +51,48 @@ class SysBaseType(object):
 
     @classmethod
     def interactiveGUI(self) -> str:
+        """Check if running in GUI mode."""
         return project.DGI.self.interactiveGUI()
 
     @classmethod
     def isUserBuild(self) -> bool:
+        """Check if this build is an user build."""
         return self.version().upper().find("USER") > -1
 
     @classmethod
     def isDeveloperBuild(self) -> bool:
+        """Check if this build is a developer build."""
         return self.version().upper().find("DEVELOPER") > -1
 
     @classmethod
     def isNebulaBuild(self) -> bool:
+        """Check if this build is a nebula build."""
         return self.version().upper().find("NEBULA") > -1
 
     @classmethod
     def isDebuggerMode(self) -> bool:
+        """Check if running in debugger mode."""
         return bool(config.value("application/isDebuggerMode", False))
 
     @classmethod
     @decorators.NotImplementedWarn
     def isCloudMode(self) -> bool:
+        """Check if running on cloud mode."""
         return False
 
     @classmethod
     def isDebuggerEnabled(self) -> bool:
+        """Check if this debugger is on."""
         return bool(config.value("application/dbadmin_enabled", False))
 
     @classmethod
     def isQuickBuild(self) -> bool:
+        """Check if this build is a Quick build."""
         return not self.isDebuggerEnabled()
 
     @classmethod
     def isLoadedModule(self, modulename: str) -> bool:
+        """Check if a module has been loaded."""
         if project.conn is None:
             raise Exception("Project is not connected yet")
         return modulename in project.conn.managerModules().listAllIdModules()
@@ -86,7 +100,7 @@ class SysBaseType(object):
     @classmethod
     def osName(self) -> str:
         """
-        Devuelve el sistema operativo sobre el que se ejecuta el programa
+        Get operating system name.
 
         @return Código del sistema operativo (WIN32, LINUX, MACX)
         """
@@ -101,35 +115,41 @@ class SysBaseType(object):
 
     @classmethod
     def nameBD(self) -> Any:
+        """Get database name."""
         if project.conn is None:
             raise Exception("Project is not connected yet")
         return project.conn.DBName()
 
     @classmethod
     def toUnicode(self, val: str, format: str) -> str:
+        """Convert string to unicode."""
         return val.encode(format).decode("utf-8", "replace")
 
     @classmethod
     def fromUnicode(self, val, format) -> Any:
+        """Convert from unicode to string."""
         return val.encode("utf-8").decode(format, "replace")
 
     @classmethod
     def Mr_Proper(self) -> None:
+        """Cleanup database like Mr. Proper."""
         if project.conn is None:
             raise Exception("Project is not connected yet")
         project.conn.Mr_Proper()
 
     @classmethod
     def installPrefix(self) -> str:
-
+        """Get folder where app is installed."""
         return filedir("..")
 
     @classmethod
     def version(self) -> str:
+        """Get version number as string."""
         return str(project.version)
 
     @classmethod
     def processEvents(self) -> None:
+        """Process event loop."""
         if not project._DGI:
             raise Exception("project._DGI is empty!")
 
@@ -137,7 +157,7 @@ class SysBaseType(object):
 
     @classmethod
     def write(self, encode_: str, dir_: str, contenido: str) -> None:
-
+        """Write to file."""
         b_ = contenido.encode()
         f = codecs.open(dir_, encoding=encode_, mode="wb+")
         f.write(b_.decode(encode_))
@@ -146,24 +166,28 @@ class SysBaseType(object):
 
     @classmethod
     def cleanupMetaData(self, connName="default") -> None:
+        """Clean up metadata."""
         if project.conn is None:
             raise Exception("Project is not connected yet")
         project.conn.useConn(connName).manager().cleanupMetaData()
 
     @classmethod
     def nameDriver(self, connName="default") -> Any:
+        """Get driver name."""
         if project.conn is None:
             raise Exception("Project is not connected yet")
         return project.conn.useConn(connName).driverName()
 
     @classmethod
     def nameHost(self, connName="default") -> Any:
+        """Get database host name."""
         if project.conn is None:
             raise Exception("Project is not connected yet")
         return project.conn.useConn(connName).host()
 
     @classmethod
     def addDatabase(self, *args) -> bool:
+        """Add a new database."""
         # def addDatabase(self, driver_name = None, db_name = None, db_user_name = None,
         #                 db_password = None, db_host = None, db_port = None, connName="default"):
         if project.conn is None:
@@ -201,17 +225,20 @@ class SysBaseType(object):
 
     @classmethod
     def removeDatabase(self, connName="default") -> Any:
+        """Remove a database."""
         if project.conn is None:
             raise Exception("Project is not connected yet")
         return project.conn.useConn(connName).removeConn(connName)
 
     @classmethod
     def idSession(self) -> str:
+        """Get Session ID."""
         # FIXME: Code copied from flapplication.aqApp
         return self.time_user_.toString(QtCore.Qt.ISODate)
 
     @classmethod
     def reportChanges(self, changes=None):
+        """Create a report for project changes."""
         ret = u""
         # DEBUG:: FOR-IN: ['key', 'changes']
         for key in changes:
@@ -228,6 +255,7 @@ class SysBaseType(object):
 
     @classmethod
     def diffXmlFilesDef(self, xmlOld=None, xmlNew=None):
+        """Create a Diff for XML."""
         arrOld = self.filesDefToArray(xmlOld)
         arrNew = self.filesDefToArray(xmlNew)
         ret: Dict[str, Any] = {}
@@ -256,6 +284,7 @@ class SysBaseType(object):
 
     @classmethod
     def filesDefToArray(self, xml=None):
+        """Convert Module MOD xml to array."""
         root = xml.firstChild()
         files = root.childNodes()
         ret = {}
@@ -290,27 +319,30 @@ class SysBaseType(object):
 
     @classmethod
     def textPacking(self, ext=None):
+        """Determine if file is text."""
         return (
-            ext.endswith(u".ui")
-            or ext.endswith(u".qry")
-            or ext.endswith(u".kut")
-            or ext.endswith(u".jrxml")
-            or ext.endswith(u".ar")
-            or ext.endswith(u".mtd")
-            or ext.endswith(u".ts")
-            or ext.endswith(u".qs")
+            ext.endswith(".ui")
+            or ext.endswith(".qry")
+            or ext.endswith(".kut")
+            or ext.endswith(".jrxml")
+            or ext.endswith(".ar")
+            or ext.endswith(".mtd")
+            or ext.endswith(".ts")
+            or ext.endswith(".qs")
             or ext.endswith(".qs.py")
-            or ext.endswith(u".xml")
-            or ext.endswith(u".xpm")
-            or ext.endswith(u".svg")
+            or ext.endswith(".xml")
+            or ext.endswith(".xpm")
+            or ext.endswith(".svg")
         )
 
     @classmethod
     def binaryPacking(self, ext=None):
+        """Determine if file is binary."""
         return ext.endswith(u".qs")
 
     @classmethod
     def infoMsgBox(self, msg=None):
+        """Show information message box."""
         msg = ustr(msg)
         msg += u"\n"
         if self.interactiveGUI():
@@ -320,6 +352,7 @@ class SysBaseType(object):
 
     @classmethod
     def warnMsgBox(self, msg=None):
+        """Show Warning message box."""
         msg = ustr(msg)
         msg += u"\n"
         if self.interactiveGUI():
@@ -329,6 +362,7 @@ class SysBaseType(object):
 
     @classmethod
     def errorMsgBox(self, msg=None):
+        """Show error message box."""
         msg = ustr(msg)
         msg += u"\n"
         if self.interactiveGUI():
@@ -338,14 +372,17 @@ class SysBaseType(object):
 
     @classmethod
     def translate(self, text: str) -> str:
+        """Translate text."""
         return text
 
     @classmethod
     def _warnHtmlPopup(self, html: str, options: List) -> None:
+        """Show a fancy html popup."""
         raise Exception("not implemented.")
 
     @classmethod
     def infoPopup(self, msg: Optional[str] = None):
+        """Show information popup."""
         msg = ustr(msg)
         caption = self.translate(u"AbanQ Información")
         msg = msg.replace("\n", "<br>")
@@ -354,6 +391,7 @@ class SysBaseType(object):
 
     @classmethod
     def warnPopup(self, msg=None):
+        """Show warning popup."""
         msg = ustr(msg)
         msg = msg.replace("\n", "<br>")
         caption = self.translate(u"AbanQ Aviso")
@@ -362,6 +400,7 @@ class SysBaseType(object):
 
     @classmethod
     def errorPopup(self, msg=None):
+        """Show error popup."""
         msg = ustr(msg)
         msg = msg.replace("\n", "<br>")
         caption = self.translate(u"AbanQ Error")
@@ -370,6 +409,7 @@ class SysBaseType(object):
 
     @classmethod
     def trTagText(self, tagText=None):
+        """Process QT_TRANSLATE_NOOP tags."""
         if not tagText.startswith(u"QT_TRANSLATE_NOOP"):
             return tagText
         txt = tagText[len("QT_TRANSLATE_NOOP") + 1 :]
@@ -379,6 +419,7 @@ class SysBaseType(object):
 
     @classmethod
     def updatePineboo(self):
+        """Execute auto-updater."""
         QMessageBox.warning(
             QApplication.focusWidget(), "Pineboo", self.translate(u"Funcionalidad no soportada aún en Pineboo."), QMessageBox.Ok
         )
@@ -386,6 +427,7 @@ class SysBaseType(object):
 
     @classmethod
     def setObjText(self, container=None, component=None, value=None):
+        """Set text to random widget."""
         c = self.testObj(container, component)
         if c is None:
             return False
@@ -405,6 +447,7 @@ class SysBaseType(object):
 
     @classmethod
     def disableObj(self, container=None, component=None):
+        """Disable random widget."""
         c = self.testObj(container, component)
         if not c:
             return False
@@ -422,6 +465,7 @@ class SysBaseType(object):
 
     @classmethod
     def enableObj(self, container=None, component=None):
+        """Enable random widget."""
         c = self.testObj(container, component)
         if not c:
             return False
@@ -438,6 +482,7 @@ class SysBaseType(object):
 
     @classmethod
     def filterObj(self, container=None, component=None, filter=None):
+        """Apply filter to random widget."""
         c = self.testObj(container, component)
         if not c:
             return False
@@ -452,6 +497,7 @@ class SysBaseType(object):
 
     @classmethod
     def testObj(self, container=None, component=None):
+        """Test if object does exist."""
         if not container or container is None:
             return False
         c = container.child(component)
@@ -462,6 +508,7 @@ class SysBaseType(object):
 
     @classmethod
     def testAndRun(self, container=None, component=None, method=None, param=None):
+        """Test and execute object."""
         c = self.testObj(container, component)
         if not c:
             return False
@@ -471,6 +518,7 @@ class SysBaseType(object):
 
     @classmethod
     def runObjMethod(self, container=None, component=None, method=None, param=None):
+        """Execute method from object."""
         c = container.child(component)
         m = getattr(c, method, None)
         if m is not None:
@@ -482,6 +530,7 @@ class SysBaseType(object):
 
     @classmethod
     def connectSS(self, ssSender=None, ssSignal=None, ssReceiver=None, ssSlot=None):
+        """Connect signal to slot."""
         if not ssSender:
             return False
         connections.connect(ssSender, ssSignal, ssReceiver, ssSlot)
@@ -489,6 +538,7 @@ class SysBaseType(object):
 
     @classmethod
     def openUrl(self, url=None):
+        """Open given URL in a browser."""
         if not url:
             return False
         os_name = self.osName()
@@ -523,6 +573,7 @@ class SysBaseType(object):
 
     @classmethod
     def launchCommand(self, comando):
+        """Execute a program."""
         try:
             Process.execute(comando)
             return True
