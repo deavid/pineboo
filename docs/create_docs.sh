43cb177a2eafca1ca630be2523b38a073daefa1b
David Martinez Marti <deavidsedice@gmail.com>
2019-08-03 14:17:30 +0100

    sphinx: add caching

--- docs/create_docs.sh
+++ docs/create_docs.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
 if [ "$1" = "" ]; then
-    rm source/code/pineboolib -R
+    test -e source/code/pineboolib && mv source/code/pineboolib source/code/old.pineboolib
+
     # Create folders
     (cd .. && git ls-files -- pineboolib/"*.py") | sed 's|/[^/]*$||' | sort -u | awk '{ print "source/code/" $0}' | xargs mkdir
 
-    (cd .. && git ls-files -- pineboolib/"*.py") | sed 's|\.py||' | sed 's|/__init__$|/index|' | sort -u | xargs -n1 $0 tpl
     (cd .. && git ls-files -- pineboolib/"*.py") | sed 's|\.py||' | sed 's|/__init__$|/index|' | sort -u | awk '{ print "   code/" $0 }' > index_template.rst
+
+    (cd .. && git ls-files -- pineboolib/"*.py") | sed 's|\.py||' | sed 's|/__init__$|/index|' | sort -u | xargs -n1 $0 tpl
+
+    test -e source/code/old.pineboolib && rm source/code/old.pineboolib -R
 
 fi
 
 if [ "$1" = "tpl" ]; then
     # Process single file template
     PARAM2=$2
+    DSTOLDFILE="source/code/old.$PARAM2.rst"
     DSTFILE="source/code/$PARAM2.rst"
     # if file exists, skip. Don't overwrite.
     # test -f "$DSTFILE" && exit 0
@@ -34,6 +39,11 @@ if [ "$1" = "tpl" ]; then
         grep -E "code/$MODPATH/[^/]+(/index)?$" index_template.rst | grep "index" | grep -v "code/$MODPATH/index" | sed "s|code/$MODPATH/||" >> "$DSTFILE"
         grep -E "code/$MODPATH/[^/]+(/index)?$" index_template.rst | grep -v "index" | grep -v "code/$MODPATH/index" | sed "s|code/$MODPATH/||" >> "$DSTFILE"
     fi
-
+    if cmp --silent "$DSTOLDFILE" "$DSTFILE"; then
+        # If files have the same contents, delete new and move original
+        # this serves to avoid changing the modification times, so other tools may cache.
+        rm "$DSTFILE"
+        mv "$DSTOLDFILE" "$DSTFILE"
+    fi
 
 fi
--- docs/update_linters.sh
+++ docs/update_linters.sh
@@ -2,26 +2,36 @@
 LINTER=$1
 APP=$2
 if [ "$LINTER" = "pylint" ]; then
+    if test -f source/linters/pylint/static/pylint_$APP.html; then
+        # If we have a cache of previous run, check if we need to update:
+        if find ../pineboolib/$APP -type f -name "*.py" \
+            -newer source/linters/pylint/static/pylint_$APP.html | \
+            head -n1 | grep pineboolib; then
+            echo "Changed files found for pineboolib.$APP."
+        else
+            echo "Cache for pineboolib.$APP is still valid."
+            exit 0;
+        fi
+    fi
+
     (cd .. && pylint pineboolib/$APP --load-plugins=pylint_json2html --output-format=jsonextended) \
         | pylint-json2html -f jsonextended -o source/linters/pylint/static/pylint_$APP.html;
     sed -e 's|body {|#pylint { font-size: 12px;|' -e 's|<body>|<div id="pylint">|' \
-        -e 's|</body>|</div>|' -i source/linters/pylint/static/pylint_$APP.html       
+        -e 's|</body>|</div>|' -i source/linters/pylint/static/pylint_$APP.html
     exit 0;
 fi
 PACKAGES=$( (cd ../pineboolib && find * -maxdepth 0 -type d \! -iname "_*") )
 # ----
 echo "Running PyLint . . ."
-echo $PACKAGES | xargs -t -n1 -P8 $0 pylint
+echo $PACKAGES | xargs -n1 -P8 $0 pylint
 # ----
 echo "Running MyPy . . ."
 (cd .. && mypy -p pineboolib --html-report=docs/source/_static/linters/mypy)
 cp source/_static/linters/mypy-html-tpl.css source/_static/linters/mypy/mypy-html.css
 # ----
 echo "Running Coverage . . ."
-(cd .. && pytest --cov pineboolib/)
-(cd .. && coverage html)
-cp -R ../htmlcov/* source/_static/linters/pytest-coverage/
-rm -R ../htmlcov
+(cd .. && pytest -q --cov=pineboolib --cov-report= pineboolib/)
+(cd .. && coverage html -d source/_static/linters/pytest-coverage/)
 cp source/_static/linters/pytest-coverage-style-tpl.css source/_static/linters/pytest-coverage/style.css
 # ----
 echo "Running Bandit . . ."
