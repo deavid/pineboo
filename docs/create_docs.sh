19cdf7e6f9da902ce53ef182f8192297fa1cafd6
David Martinez Marti <deavidsedice@gmail.com>
2019-08-03 14:24:15 +0100

    sphinx: create pineboolib folder

--- docs/create_docs.sh
+++ docs/create_docs.sh
@@ -2,7 +2,7 @@
 
 if [ "$1" = "" ]; then
     test -e source/code/pineboolib && mv source/code/pineboolib source/code/old.pineboolib
-
+    mkdir source/code/pineboolib
     # Create folders
     (cd .. && git ls-files -- pineboolib/"*.py") | sed 's|/[^/]*$||' | sort -u | awk '{ print "source/code/" $0}' | xargs mkdir
 
