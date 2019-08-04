dcd550e12466c669fe49179fde50c9fe4cd277b0
David Martinez Marti <deavidsedice@gmail.com>
2019-08-04 16:21:48 +0100

    Add test_import_safety linter

---
+++ test_import_safety.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Tests that all python modules under pineboolib can be imported isolated and separately.
+
+if [ "$1" == "" ]; then
+    rm tempdata/import_safety_*.log
+    find pineboolib -type f -iname "*.py" | sed -e 's|/__init__\.py$||' -e 's|\.py||' | tr '/' '.' \
+        | xargs -n1 -P8 "$0"
+    exit $?;  # Return with same error code
+fi
+
+PINEBOO_MODULE=$1
+OUTPUT_FILE="tempdata/import_safety_${PINEBOO_MODULE//[.]/_}.log"
+RESULT=$(python3 -c "import $PINEBOO_MODULE" 2>&1)
+EXIT_CODE="$?"
+if [ "$EXIT_CODE" != "0" ]; then
+    echo "Failed to import $PINEBOO_MODULE" > "$OUTPUT_FILE"
+    echo "Exit code: $EXIT_CODE" >> "$OUTPUT_FILE"
+    echo "---" >> "$OUTPUT_FILE"
+    echo -ne "$RESULT" >> "$OUTPUT_FILE"
+    cat "$OUTPUT_FILE"
+    exit 255
+fi
