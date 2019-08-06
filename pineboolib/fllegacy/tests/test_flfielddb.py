a72080984ee79e8191909d5d1b01a919ed2296b6
José A. Fernández <aullasistemas@gmail.com>
2019-08-06 12:13:30 +0200

    nuevos test fllegacy

---
+++ pineboolib/fllegacy/tests/test_flfielddb.py
@@ -0,0 +1,44 @@
+"""Test_flfieldDB module."""
+
+import unittest
+from pineboolib.loader.main import init_testing
+
+
+class TestFLFieldDBString(unittest.TestCase):
+    """TestFLFieldDBString Class."""
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        """Ensure pineboo is initialized for testing."""
+        init_testing()
+
+    def test_basic(self) -> None:
+        """Test string FLFieldDB mode."""
+
+        from pineboolib.qsa import dictmodules
+        from pineboolib.application.database import pnsqlcursor
+
+        cursor_1 = pnsqlcursor.PNSqlCursor("flmodules")
+        cursor_1.select()
+        cursor_1.setModeAccess(cursor_1.Insert)
+        cursor_1.refreshBuffer()
+        cursor_1.insertRecord(False)
+
+        module_ = dictmodules.from_project("formRecordflmodules")
+        self.assertNotEqual(module_, None)
+
+        cursor_2 = module_.cursor()
+        field = module_.child("fLFieldDB2")
+        self.assertNotEqual(field, None)
+        print(field, module_.child)
+        field.setValue("hola")
+
+        self.assertEqual(cursor_1.valueBuffer("descripcion"), "hola")
+        cursor_2.setValueBuffer("descripcion", "nueva hola.")
+        self.assertEqual(field.value(), "nueva hola.")
+
+        # module_.form.close()
+
+
+if __name__ == "__main__":
+    unittest.main()
