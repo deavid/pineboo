bc8b91065a064abaffd6cebc7507f9d987efcd76
José A. Fernández <aullasistemas@gmail.com>
2019-08-07 14:16:27 +0200

    test_process

---
+++ pineboolib/application/tests/test_process.py
@@ -0,0 +1,47 @@
+"""Test_process module."""
+
+import unittest
+from pineboolib.loader.main import init_testing
+
+
+class TestProcess(unittest.TestCase):
+    """TestProcess Class."""
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        """Ensure pineboo is initialized for testing."""
+        init_testing()
+
+    def test_Process(self) -> None:
+        """Test Process."""
+
+        from pineboolib.application import process
+
+        proc = process.Process()
+
+        proc.execute("java -version")
+
+        salida = None
+        if proc.stderr != "":
+            salida = proc.stderr
+        else:
+            salida = proc.stdout
+
+        self.assertTrue(salida.find("java") > -1)
+
+    def test_ProcessStatic(self) -> None:
+        """Test ProcessStatic."""
+
+        from pineboolib.qsa import qsa
+
+        proc = qsa.ProcessStatic
+
+        proc.execute("java -version")
+
+        salida = None
+        if proc.stderr != "":
+            salida = proc.stderr
+        else:
+            salida = proc.stdout
+
+        self.assertTrue(salida.find("java") > -1)
